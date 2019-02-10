d307500a0264107acf8c844d7f09db6990ba3dc1
Michael Cohen <scudette@gmail.com>
2019-02-10 12:59:11 +1000

    Replaced EVTX parser with Velocidex one.

--- vql/parsers/evtx.go
+++ vql/parsers/evtx.go
@@ -18,31 +18,14 @@
 package parsers
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
-	"io"
 	"time"
 
-	"github.com/0xrawsec/golang-evtx/evtx"
-	"github.com/0xrawsec/golang-utils/encoding"
+	"www.velocidex.com/golang/evtx"
 	"www.velocidex.com/golang/velociraptor/glob"
 	vql_subsystem "www.velocidex.com/golang/velociraptor/vql"
 	vfilter "www.velocidex.com/golang/vfilter"
 )
-
-// Get rid of the complex goevtx object and just return a
-// map[string]interface{}
-func Normalize(event *evtx.GoEvtxMap) (map[string]interface{}, error) {
-	encoded, err := json.Marshal(event)
-	if err != nil {
-		return nil, err
-	}
-
-	result := make(map[string]interface{})
-	err = json.Unmarshal(encoded, &result)
-	return result, err
-}
 
 type _ParseEvtxPluginArgs struct {
 	Filenames []string `vfilter:"required,field=filename"`
@@ -50,83 +33,6 @@ type _ParseEvtxPluginArgs struct {
 }
 
 type _ParseEvtxPlugin struct{}
-
-func _WriteEvents(
-	scope *vfilter.Scope,
-	file io.ReadSeeker,
-	output_chan chan vfilter.Row,
-	first_event int64) (
-	last_event int64, err error) {
-	header := &evtx.FileHeader{}
-	err = encoding.Unmarshal(file, header, evtx.Endianness)
-	if err != nil {
-		return
-	}
-
-	last_event = first_event
-
-	for i := int64(0); ; i++ {
-		offsetChunk := int64(header.ChunkDataOffset) +
-			int64(evtx.ChunkSize)*i
-
-		chunk := evtx.NewChunk()
-		chunk.Offset = offsetChunk
-		chunk.Data = make([]byte, evtx.ChunkSize)
-
-		offset, err := file.Seek(offsetChunk, io.SeekStart)
-		if err != nil {
-			continue
-		}
-		if offset != offsetChunk || err != nil {
-			return last_event, nil
-		}
-
-		n, err := io.ReadAtLeast(file, chunk.Data, len(chunk.Data))
-		if n != len(chunk.Data) || err != nil {
-			return last_event, nil
-		}
-
-		chunk_reader := bytes.NewReader(chunk.Data)
-		chunk.ParseChunkHeader(chunk_reader)
-
-		if chunk.Header.LastEventRecID <= first_event {
-			continue
-		}
-
-		chunk_reader.Seek(
-			int64(chunk.Header.SizeHeader),
-			io.SeekStart)
-		chunk.ParseStringTable(chunk_reader)
-		err = chunk.ParseTemplateTable(chunk_reader)
-		if err != nil {
-			continue
-		}
-
-		err = chunk.ParseEventOffsets(chunk_reader)
-		if err != nil {
-			continue
-		}
-
-		for _, event_offset := range chunk.EventOffsets {
-			event := chunk.ParseEvent(int64(event_offset))
-			item, err := event.GoEvtxMap(&chunk)
-			if err == nil {
-				if item.EventRecordID() > last_event {
-					event, err := Normalize(item)
-					if err == nil {
-						event_details, pres := event["Event"]
-						if pres && output_chan != nil {
-							output_chan <- event_details
-						}
-					}
-					last_event = item.EventRecordID()
-				}
-			}
-		}
-	}
-
-	return
-}
 
 func (self _ParseEvtxPlugin) Call(
 	ctx context.Context,
@@ -147,19 +53,31 @@ func (self _ParseEvtxPlugin) Call(
 		for _, filename := range arg.Filenames {
 			func() {
 				accessor := glob.GetAccessor(arg.Accessor, ctx)
-				file, err := accessor.Open(filename)
+				fd, err := accessor.Open(filename)
 				if err != nil {
 					scope.Log("Unable to open file %s: %v",
 						filename, err)
 					return
 				}
-				defer file.Close()
+				defer fd.Close()
 
-				_, err = _WriteEvents(scope, file, output_chan, 0)
+				chunks, err := evtx.GetChunks(fd)
 				if err != nil {
-					scope.Log("Error: %v", err)
+					scope.Log("Unable to parse file %s: %v",
+						filename, err)
 					return
 				}
+
+				for _, chunk := range chunks {
+					records, _ := chunk.Parse(0)
+					for _, i := range records {
+						event_map, ok := i.Event.(map[string]interface{})
+						if ok {
+							output_chan <- event_map["Event"]
+						}
+					}
+				}
+
 			}()
 		}
 	}()
@@ -194,47 +112,69 @@ func (self _WatchEvtxPlugin) Call(
 		}
 
 		accessor := glob.GetAccessor(arg.Accessor, ctx)
-		event_counts := make(map[string]int64)
+		event_counts := make(map[string]uint64)
 
 		// Parse the files once to get the last event
 		// id. After this we will watch for new events added
 		// to the file.
 		for _, filename := range arg.Filenames {
 			func() {
-				file, err := accessor.Open(filename)
+				fd, err := accessor.Open(filename)
 				if err != nil {
 					return
 				}
-				defer file.Close()
+				defer fd.Close()
 
-				last_event, err := _WriteEvents(
-					scope, file, nil, 0)
-
-				if err == nil {
-					event_counts[filename] = last_event
+				chunks, err := evtx.GetChunks(fd)
+				if err != nil {
+					return
 				}
+				last_event := uint64(0)
+				for _, c := range chunks {
+					if c.Header.LastEventRecID > last_event {
+						last_event = c.Header.LastEventRecID
+					}
+				}
+				event_counts[filename] = last_event
 			}()
 		}
 
 		for {
 			for _, filename := range arg.Filenames {
 				func() {
-					file, err := accessor.Open(filename)
+					fd, err := accessor.Open(filename)
 					if err != nil {
 						scope.Log("Unable to open file %s: %v",
 							filename, err)
 						return
 					}
-					defer file.Close()
-					last_event, err := _WriteEvents(
-						scope, file, output_chan,
-						event_counts[filename])
+					defer fd.Close()
+
+					last_event := event_counts[filename]
+					chunks, err := evtx.GetChunks(fd)
 					if err != nil {
-						scope.Log("Error: %v", err)
 						return
 					}
 
-					event_counts[filename] = last_event
+					new_last_event := last_event
+					for _, c := range chunks {
+						if c.Header.LastEventRecID <= last_event {
+							continue
+						}
+
+						records, _ := c.Parse(int(last_event))
+						for _, record := range records {
+							if record.Header.RecordID > new_last_event {
+								new_last_event = record.Header.RecordID
+							}
+							event_map, ok := record.Event.(map[string]interface{})
+							if ok {
+								output_chan <- event_map["Event"]
+							}
+						}
+					}
+
+					event_counts[filename] = new_last_event
 				}()
 			}
 
