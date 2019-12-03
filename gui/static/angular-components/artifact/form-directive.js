850baa8575869c9b3a5cbb43556b748a00277b7f
Michael Cohen <scudette@gmail.com>
2019-12-03 14:05:05 +1000

    Added a selector parameter type to the GUI.

--- gui/static/angular-components/artifact/form-directive.js
+++ gui/static/angular-components/artifact/form-directive.js
@@ -6,16 +6,23 @@ const FormController = function($scope) {
   /** @private {!angular.Scope} */
   this.scope_ = $scope;
   this.popup_opened = false;
-  this.date_time;
+    this.date_time;
+
+    this.dateOptions = {allowInvalid: true};
+    this.altInputFormats = ["M!/d!/yyyy"];
   $scope.$watch('controller.date_time',
                 this.onDateChange_.bind(this));
 };
 
 
 FormController.prototype.onDateChange_ = function() {
-  if (angular.isDefined(this.date_time)) {
-    this.scope_.value[this.scope_.field] = (
-      this.date_time.getTime() / 1000).toString();
+    if (angular.isDefined(this.date_time)) {
+        try {
+            this.scope_.value[this.scope_.field] = (
+                this.date_time.getTime() / 1000).toString();
+        } catch(e) {
+            this.scope_.value[this.scope_.field] = this.date_time;
+        }
   }
 };
 
@@ -27,10 +34,11 @@ exports.FormDirective = function() {
   return {
     restrict: 'E',
     scope: {
-      field: '=',
-      value: '=',
-      type: '=',
-      description: '=',
+        field: '=',
+        value: '=',
+        info: '=',
+        type: '=',
+        description: '=',
     },
     templateUrl: '/static/angular-components/artifact/form.html',
     controller: FormController,
