56bf598971039bfbc82d1e101237e393753958ab
Michael Cohen <scudette@gmail.com>
2020-01-11 19:09:34 +1000

    Release 0.3.8

--- constants/constants.go
+++ constants/constants.go
@@ -20,7 +20,7 @@ package constants
 import "regexp"
 
 const (
-	VERSION                    = "0.3.7"
+	VERSION                    = "0.3.8"
 	ENROLLMENT_WELL_KNOWN_FLOW = "E:Enrol"
 	MONITORING_WELL_KNOWN_FLOW = FLOW_PREFIX + "Monitoring"
 
