8cacace3fcd158c7dac49753ca5f9fdc48cc693d
Michael Cohen <scudette@gmail.com>
2020-03-09 21:57:22 +1000

    Added remediation artifacts.
    Prepare release 0.4.0

--- constants/constants.go
+++ constants/constants.go
@@ -20,7 +20,7 @@ package constants
 import "regexp"
 
 const (
-	VERSION                    = "0.3.9"
+	VERSION                    = "0.4.0"
 	ENROLLMENT_WELL_KNOWN_FLOW = "E:Enrol"
 	MONITORING_WELL_KNOWN_FLOW = FLOW_PREFIX + "Monitoring"
 
