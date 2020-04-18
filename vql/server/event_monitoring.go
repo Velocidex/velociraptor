c1ead4c7f53e8ea5eaab19498a53a35f59d5768a
Michael Cohen <scudette@gmail.com>
2020-04-19 01:02:13 +1000

    Fix test.

--- vql/server/event_monitoring.go
+++ vql/server/event_monitoring.go
@@ -155,14 +155,14 @@ func (self GetServerMonitoring) Call(
 
 	err := vql_subsystem.CheckAccess(scope, acls.SERVER_ADMIN)
 	if err != nil {
-		scope.Log("get_client_monitoring: %s", err)
+		scope.Log("get_server_monitoring: %s", err)
 		return vfilter.Null{}
 	}
 
 	arg := &GetServerMonitoringArgs{}
 	err = vfilter.ExtractArgs(scope, args, arg)
 	if err != nil {
-		scope.Log("get_client_monitoring: %v", err)
+		scope.Log("get_server_monitoring: %v", err)
 		return vfilter.Null{}
 	}
 
@@ -175,14 +175,14 @@ func (self GetServerMonitoring) Call(
 
 	client, closer, err := grpc_client.Factory.GetAPIClient(ctx, config_obj)
 	if err != nil {
-		scope.Log("get_client_monitoring: %s", err.Error())
+		scope.Log("get_server_monitoring: %s", err.Error())
 		return vfilter.Null{}
 	}
 	defer closer()
 
 	response, err := client.GetServerMonitoringState(ctx, &empty.Empty{})
 	if err != nil {
-		scope.Log("get_client_monitoring: %s", err.Error())
+		scope.Log("get_server_monitoring: %s", err.Error())
 		return vfilter.Null{}
 	}
 
@@ -191,7 +191,7 @@ func (self GetServerMonitoring) Call(
 
 func (self GetServerMonitoring) Info(scope *vfilter.Scope, type_map *vfilter.TypeMap) *vfilter.FunctionInfo {
 	return &vfilter.FunctionInfo{
-		Name:    "get_client_monitoring",
+		Name:    "get_server_monitoring",
 		Doc:     "Retrieve the current client monitoring state.",
 		ArgType: type_map.AddType(scope, &GetServerMonitoringArgs{}),
 	}
@@ -210,14 +210,14 @@ func (self SetServerMonitoring) Call(
 
 	err := vql_subsystem.CheckAccess(scope, acls.SERVER_ADMIN)
 	if err != nil {
-		scope.Log("set_client_monitoring: %s", err)
+		scope.Log("set_server_monitoring: %s", err)
 		return vfilter.Null{}
 	}
 
 	arg := &SetServerMonitoringArgs{}
 	err = vfilter.ExtractArgs(scope, args, arg)
 	if err != nil {
-		scope.Log("set_client_monitoring: %v", err)
+		scope.Log("set_server_monitoring: %v", err)
 		return vfilter.Null{}
 	}
 
@@ -236,7 +236,7 @@ func (self SetServerMonitoring) Call(
 	default:
 		serialized, err := json.Marshal(arg.Data)
 		if err != nil {
-			scope.Log("set_client_monitoring: %v", err)
+			scope.Log("set_server_monitoring: %v", err)
 			return vfilter.Null{}
 		}
 		value_json = string(serialized)
@@ -246,20 +246,20 @@ func (self SetServerMonitoring) Call(
 	value := &flows_proto.ArtifactCollectorArgs{}
 	err = json.Unmarshal([]byte(value_json), value)
 	if err != nil {
-		scope.Log("set_client_monitoring: %v", err)
+		scope.Log("set_server_monitoring: %v", err)
 		return vfilter.Null{}
 	}
 
 	client, closer, err := grpc_client.Factory.GetAPIClient(ctx, config_obj)
 	if err != nil {
-		scope.Log("set_client_monitoring: %s", err.Error())
+		scope.Log("set_server_monitoring: %s", err.Error())
 		return vfilter.Null{}
 	}
 	defer closer()
 
 	response, err := client.SetServerMonitoringState(ctx, value)
 	if err != nil {
-		scope.Log("set_client_monitoring: %s", err.Error())
+		scope.Log("set_server_monitoring: %s", err.Error())
 		return vfilter.Null{}
 	}
 
@@ -268,8 +268,8 @@ func (self SetServerMonitoring) Call(
 
 func (self SetServerMonitoring) Info(scope *vfilter.Scope, type_map *vfilter.TypeMap) *vfilter.FunctionInfo {
 	return &vfilter.FunctionInfo{
-		Name:    "set_client_monitoring",
-		Doc:     "Sets the current client monitoring state.",
+		Name:    "set_server_monitoring",
+		Doc:     "Sets the current server monitoring state.",
 		ArgType: type_map.AddType(scope, &SetServerMonitoringArgs{}),
 	}
 }
