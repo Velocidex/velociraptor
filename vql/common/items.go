6924479c905b21c9382e20d46fb699c251e3f71d
Michael Cohen <scudette@gmail.com>
2020-06-03 22:45:02 +1000

    Added items and tempdir VQL functions.

---
+++ vql/common/items.go
@@ -0,0 +1,79 @@
+package common
+
+import (
+	"context"
+	"reflect"
+
+	"github.com/Velocidex/ordereddict"
+	vql_subsystem "www.velocidex.com/golang/velociraptor/vql"
+	vfilter "www.velocidex.com/golang/vfilter"
+)
+
+type ItemsPluginArgs struct {
+	Item vfilter.Any `vfilter:"optional,field=item,doc=The item to enumerate."`
+}
+
+type ItemsPlugin struct{}
+
+func (self ItemsPlugin) Call(
+	ctx context.Context,
+	scope *vfilter.Scope,
+	args *ordereddict.Dict) <-chan vfilter.Row {
+	output_chan := make(chan vfilter.Row)
+
+	go func() {
+		defer close(output_chan)
+
+		arg := &ItemsPluginArgs{}
+		err := vfilter.ExtractArgs(scope, args, arg)
+		if err != nil {
+			scope.Log("items: %v", err)
+			return
+		}
+
+		switch t := arg.Item.(type) {
+		case vfilter.LazyExpr:
+			arg.Item = t.Reduce()
+		}
+
+		a_value := reflect.Indirect(reflect.ValueOf(arg.Item))
+		a_type := a_value.Type()
+
+		if a_type.Kind() == reflect.Slice {
+			for i := 0; i < a_value.Len(); i++ {
+				element := a_value.Index(i).Interface()
+				output_chan <- ordereddict.NewDict().
+					Set("_key", i).
+					Set("_value", element)
+			}
+			return
+		}
+
+		members := scope.GetMembers(arg.Item)
+		if len(members) > 0 {
+			for _, key := range members {
+				value, pres := scope.Associative(arg.Item, key)
+				if pres {
+					output_chan <- ordereddict.NewDict().
+						Set("_key", key).
+						Set("_value", value)
+				}
+			}
+		}
+
+	}()
+
+	return output_chan
+}
+
+func (self ItemsPlugin) Info(scope *vfilter.Scope, type_map *vfilter.TypeMap) *vfilter.PluginInfo {
+	return &vfilter.PluginInfo{
+		Name:    "items",
+		Doc:     "Enumerate all members of the item (similar to Pythons items() method.",
+		ArgType: type_map.AddType(scope, &ItemsPluginArgs{}),
+	}
+}
+
+func init() {
+	vql_subsystem.RegisterPlugin(&ItemsPlugin{})
+}
