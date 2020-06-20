b2f23c6f47a65e97bf0765e4addae13661115963
mgreen27 <mgreen27@gmail.com>
2020-06-20 01:05:15 +0000

    Add VSS tests

--- artifacts/testdata/windows/init.cmd
+++ artifacts/testdata/windows/init.cmd
@@ -1,1 +1,11 @@
 mklink "c:\users\link" "c:\Windows"
+
+# VSS testing.
+echo "test" > c:\Users\test.txt
+echo "test2" > c:\Users\test2.txt
+sc.exe create TestingDetection1 binPath="%COMSPEC% /Q /c echo 'COMSPEC testing 1"
+wmic shadowcopy call create Volume='C:'
+wevtutil.exe cl System
+echo "test2" >> c:\Users\test2.txt
+sc.exe create TestingDetection2 binPath="%COMSPEC% /Q /c echo 'COMSPEC testing 2"
+wmic shadowcopy call create Volume='C:'
