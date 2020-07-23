709aa1ed16692a35e9e811bbca90984bf8ea2ef3
Michael Cohen <scudette@gmail.com>
2020-07-24 02:50:30 +1000

    Set time.Local atomically.

--- bin/time.go
+++ bin/time.go
@@ -1,8 +1,15 @@
 package main
 
-import "time"
+import (
+	"sync/atomic"
+	"time"
+	"unsafe"
+)
 
 func init() {
 	// Default time is UTC - set atomically.
-	time.Local = time.UTC
+	utc_time := unsafe.Pointer(time.UTC)
+	local_time := (*unsafe.Pointer)(unsafe.Pointer(&time.Local))
+
+	atomic.StorePointer(local_time, utc_time)
 }
