b726f06d3930c672daa9bd9cbff573af3cc78b99
Michael Cohen <scudette@gmail.com>
2020-09-02 13:49:10 +1000

    Update version for release.

--- constants/constants.go
+++ constants/constants.go
@@ -20,7 +20,7 @@ package constants
 import "regexp"
 
 const (
-	VERSION                    = "0.4.8"
+	VERSION                    = "0.4.9"
 	ENROLLMENT_WELL_KNOWN_FLOW = "E:Enrol"
 	MONITORING_WELL_KNOWN_FLOW = FLOW_PREFIX + "Monitoring"
 
