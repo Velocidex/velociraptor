d3df68350c797273df19f26060d55538230de577
Michael Cohen <scudette@gmail.com>
2020-11-20 17:51:37 +1000

    Create a new client record if required.

--- bin/import.go
+++ bin/import.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"archive/zip"
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
 	"strings"
 	"time"
 
 	"github.com/Velocidex/ordereddict"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
+	actions_proto "www.velocidex.com/golang/velociraptor/actions/proto"
+	"www.velocidex.com/golang/velociraptor/api"
+	config_proto "www.velocidex.com/golang/velociraptor/config/proto"
+	"www.velocidex.com/golang/velociraptor/constants"
 	"www.velocidex.com/golang/velociraptor/datastore"
 	"www.velocidex.com/golang/velociraptor/file_store"
 	"www.velocidex.com/golang/velociraptor/file_store/result_sets"
@@ -23,13 +29,87 @@ var (
 	// Command line interface for VQL commands.
 	import_cmd    = app.Command("import", "Import an offline collection to a client")
 	import_client = import_cmd.Flag("client_id", "The client id to import the client for.").
-			Required().String()
+			String()
+
+	import_hostname = import_cmd.Flag("hostname", "The hostname to import the client for.").
+			String()
+
+	import_create_host = import_cmd.Flag("create", "Create a client id record if needed.").
+				Bool()
 
 	import_file = import_cmd.Arg("filename", "The offline zip file to import").
 			Required().String()
 )
 
+// Generate a new client id
+func NewClientId() string {
+	buf := make([]byte, 8)
+	_, _ = rand.Read(buf)
+	dst := make([]byte, hex.EncodedLen(8))
+	hex.Encode(dst, buf)
+	return "C." + string(dst)
+}
+
+// Retrieve the client id from the hostname.
+func getClientId(config_obj *config_proto.Config,
+	hostname string) string {
+	db, err := datastore.GetDB(config_obj)
+	if err != nil {
+		return ""
+	}
+
+	for _, client_id := range db.SearchClients(
+		config_obj, constants.CLIENT_INDEX_URN,
+		hostname, "", 0, 1, datastore.SORT_UP) {
+		return client_id
+	}
+	return ""
+}
+
+// Create a new client record
+func makeNewClient(
+	config_obj *config_proto.Config,
+	hostname string) (string, error) {
+	client_id := NewClientId()
+	client_info := &actions_proto.ClientInfo{
+		ClientId:     client_id,
+		Hostname:     hostname,
+		Fqdn:         hostname,
+		Architecture: "Offline",
+		ClientName:   "OfflineVelociraptor",
+	}
+
+	db, err := datastore.GetDB(config_obj)
+	if err != nil {
+		return "", err
+	}
+
+	client_path_manager := paths.NewClientPathManager(client_id)
+	err = db.SetSubject(config_obj,
+		client_path_manager.Path(), client_info)
+	if err != nil {
+		return "", err
+	}
+	// Add the new client to the index.
+	keywords := []string{
+		"all", // This is used for "." search
+		client_id,
+		client_info.Hostname,
+		client_info.Fqdn,
+		"host:" + client_info.Hostname,
+	}
+
+	return client_id, db.SetIndex(config_obj,
+		constants.CLIENT_INDEX_URN,
+		client_id, keywords,
+	)
+}
+
 func doImport() {
+	if *import_client == "" && *import_hostname == "" {
+		kingpin.Fatalf("One of client_id or hostname should be specified.")
+	}
+
 	config_obj, err := DefaultConfigLoader.
 		WithRequiredFrontend().
 		WithRequiredUser().
@@ -38,7 +118,6 @@ func doImport() {
 
 	ctx, cancel := install_sig_handler()
 	defer cancel()
-	_ = ctx
 
 	sm, err := startEssentialServices(config_obj)
 	kingpin.FatalIfError(err, "Load Config ")
@@ -46,6 +125,27 @@ func doImport() {
 
 	db, err := datastore.GetDB(config_obj)
 	kingpin.FatalIfError(err, "Saving file ")
+
+	client_id := *import_client
+	hostname := *import_hostname
+	if hostname != "" {
+		client_id = getClientId(config_obj, hostname)
+		if client_id == "" {
+			if !*import_create_host {
+				kingpin.Fatalf("Hostname %v not known, you can "+
+					"create one with the --create flag ", hostname)
+			}
+			client_id, err = makeNewClient(config_obj, hostname)
+			kingpin.FatalIfError(err, "Creating new client")
+		}
+	}
+
+	api_client, err := api.GetApiClient(config_obj, nil, client_id, false /* detailed */)
+	kingpin.FatalIfError(err, "Client ID Not known ")
+
+	if api_client.AgentInformation.Name == "" {
+		kingpin.Fatalf("Client ID not known")
+	}
 
 	file_store_factory := file_store.GetFileStore(config_obj)
 
@@ -55,15 +155,16 @@ func doImport() {
 	repository, err := manager.GetGlobalRepository(config_obj)
 	kingpin.FatalIfError(err, "Load Config ")
 
+	// Open the zip file we are importing.
 	zipfile, err := zip.OpenReader(*import_file)
 	kingpin.FatalIfError(err, "Loading collection file ")
 	defer zipfile.Close()
 
+	// Keep track of all the artifacts in the zip file.
 	artifacts := make(map[string]bool)
 	uploads := []string{}
 
 	// Create a new flow and path manager for it.
-	client_id := *import_client
 	flow_id := launcher.NewFlowId(client_id)
 	path_manager := paths.NewFlowPathManager(client_id, flow_id)
 	new_flow := &flows_proto.ArtifactCollectorContext{
@@ -86,6 +187,8 @@ func doImport() {
 	kingpin.FatalIfError(err, "Loading collection file ")
 	defer log_result_set.Close()
 
+	// A log function that stores messages in the flow log as well
+	// as print them to the screen.
 	log := func(format string, args ...interface{}) {
 		now := time.Now().UTC()
 
@@ -106,6 +209,10 @@ func doImport() {
 		artifact_name := strings.TrimSuffix(file.Name, ".json")
 		artifact, pres := repository.Get(config_obj, artifact_name)
 		if pres {
+			// File is an artifact result set - import it
+			// into the filestore. artifact_name is the
+			// full name include source of the artifact so
+			// we need to dedup here.
 			artifacts[artifact.Name] = true
 
 			new_flow.ArtifactsWithResults = append(new_flow.ArtifactsWithResults,
@@ -173,6 +280,8 @@ func doImport() {
 		}
 	}
 
+	// Copy all unique artifacts to the request struct - this will
+	// go into the flow context.
 	for k, _ := range artifacts {
 		new_flow.Request.Artifacts = append(new_flow.Request.Artifacts, k)
 	}
