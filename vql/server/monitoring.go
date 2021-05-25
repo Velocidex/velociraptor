7a16d817f0fdb0ecfc35486c2c115c298821ff44
Matthew Green <mgreen27@users.noreply.github.com>
2021-05-25 17:26:31 +1000

    Slight monitoring modifications (#1078)

--- vql/server/monitoring.go
+++ vql/server/monitoring.go
@@ -152,7 +152,7 @@ func (self WatchMonitoringPlugin) Call(
 		}
 
 		switch mode {
-		case paths.MODE_SERVER_EVENT, paths.MODE_CLIENT_EVENT:
+		case paths.MODE_SERVER_EVENT, paths.MODE_CLIENT_EVENT, paths.INTERNAL:
 			break
 
 		default:
