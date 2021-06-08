162a28e267be1e2a7bcdbe67857fd61fc8a6eb8e
Mike Cohen <scudette@users.noreply.github.com>
2021-06-08 13:39:45 +1000

    Updated KapeFiles Targets artifact (#1097)
    Also various cleanups.

--- gui/velociraptor/src/components/flows/flow-logs.js
+++ gui/velociraptor/src/components/flows/flow-logs.js
@@ -1,8 +1,16 @@
+import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 
 import VeloPagedTable from '../core/paged-table.js';
 import VeloTimestamp from "../utils/time.js";
+
+
+function getFlowState(flow) {
+    return {flow_id: flow.session_id,
+            total_logs: flow.total_logs};
+}
+
 
 export default class FlowLogs extends React.Component {
     static propTypes = {
@@ -37,6 +45,7 @@ export default class FlowLogs extends React.Component {
                   flow_id: this.props.flow.session_id,
                   type: "log",
               }}
+              version={getFlowState(this.props.flow)}
             />
         );
     }
