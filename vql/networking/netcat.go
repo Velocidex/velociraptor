ec27fbda540f2b2da70b5127bd8c8d155a1da1b9
Mike Cohen <scudette@users.noreply.github.com>
2021-12-06 12:47:05 +1000

    Added netcat plugin to read from socket (#1412)
    also fixed bug in watch_syslog() plugin which could not handle file rotation.

---
+++ vql/networking/netcat.go
@@ -0,0 +1,139 @@
+package networking
+
+import (
+	"context"
+	"net"
+	"strings"
+
+	"github.com/Velocidex/ordereddict"
+	"www.velocidex.com/golang/velociraptor/acls"
+	vql_subsystem "www.velocidex.com/golang/velociraptor/vql"
+	vfilter "www.velocidex.com/golang/vfilter"
+	"www.velocidex.com/golang/vfilter/arg_parser"
+)
+
+type NetcatPluginArgs struct {
+	Address     string `vfilter:"required,field=address,doc=The address to connect to (can be a file in case of a unix domain socket)"`
+	AddressType string `vfilter:"optional,field=type,doc=Can be tcp or unix (default TCP)"`
+	Send        string `vfilter:"optional,field=send,doc=Data to send before reading"`
+	Sep         string `vfilter:"optional,field=sep,doc=The separator that will be used to split (default - line feed)"`
+	Chunk       int    `vfilter:"optional,field=chunk_size,doc=Read input with this chunk size (default 64kb)"`
+}
+
+type NetcatPlugin struct{}
+
+func (self *NetcatPlugin) Call(
+	ctx context.Context,
+	scope vfilter.Scope,
+	args *ordereddict.Dict) <-chan vfilter.Row {
+	output_chan := make(chan vfilter.Row)
+
+	go func() {
+		defer close(output_chan)
+
+		arg := &NetcatPluginArgs{}
+		err := arg_parser.ExtractArgsWithContext(ctx, scope, args, arg)
+		if err != nil {
+			scope.Log("netcat: %s", err)
+			return
+		}
+
+		err = vql_subsystem.CheckAccess(scope, acls.COLLECT_SERVER)
+		if err != nil {
+			scope.Log("netcat: %s", err)
+			return
+		}
+
+		socket_type := arg.AddressType
+		switch socket_type {
+		case "":
+			socket_type = "tcp"
+		case "tcp", "unix":
+		default:
+			scope.Log("netcat: unsupported address type (%v)", arg.AddressType)
+			return
+		}
+
+		var d net.Dialer
+		conn, err := d.DialContext(ctx, socket_type, arg.Address)
+		if err != nil {
+			scope.Log("netcat: %s", err)
+			return
+		}
+		defer conn.Close()
+
+		if arg.Send != "" {
+			go func() {
+				_, err := conn.Write([]byte(arg.Send))
+				if err != nil {
+					scope.Log("netcat: %s", err)
+				}
+			}()
+		}
+
+		sep := arg.Sep
+		if sep == "" {
+			sep = "\n"
+		}
+
+		chunk_size := arg.Chunk
+		if chunk_size == 0 {
+			chunk_size = 64 * 1024
+		}
+
+		if chunk_size > 10*1024*1024 {
+			chunk_size = 10 * 1024 * 1024
+		}
+
+		buf := make([]byte, chunk_size)
+		offset := 0
+		for {
+			n, err := conn.Read(buf[offset:])
+			if err != nil {
+				return
+			}
+
+			lines := strings.Split(string(buf[:offset+n]), sep)
+			for idx, line := range lines {
+				if idx == len(lines)-1 {
+					// If last line is non empty the buffer does not
+					// end with a separator, copy the last line to the
+					// old buffer and start reading from there.
+					last_line := lines[idx]
+					if last_line != "" {
+						copy(buf, []byte(last_line))
+						offset = len(last_line)
+					}
+					continue
+				}
+
+				if line == "" {
+					continue
+				}
+
+				select {
+				case <-ctx.Done():
+					return
+
+				case output_chan <- ordereddict.NewDict().Set("Data", line):
+				}
+			}
+
+		}
+
+	}()
+
+	return output_chan
+}
+
+func (self NetcatPlugin) Info(scope vfilter.Scope, type_map *vfilter.TypeMap) *vfilter.PluginInfo {
+	return &vfilter.PluginInfo{
+		Name:    "netcat",
+		Doc:     "Make a tcp connection and read data from a socket.",
+		ArgType: type_map.AddType(scope, &NetcatPluginArgs{}),
+	}
+}
+
+func init() {
+	vql_subsystem.RegisterPlugin(&NetcatPlugin{})
+}
--- vql/parsers/syslog/watcher.go
+++ vql/parsers/syslog/watcher.go
@@ -8,7 +8,9 @@ import (
 	"time"
 
 	"github.com/Velocidex/ordereddict"
+	config_proto "www.velocidex.com/golang/velociraptor/config/proto"
 	"www.velocidex.com/golang/velociraptor/glob"
+	"www.velocidex.com/golang/velociraptor/logging"
 	"www.velocidex.com/golang/velociraptor/utils"
 	vql_subsystem "www.velocidex.com/golang/velociraptor/vql"
 	"www.velocidex.com/golang/vfilter"
@@ -20,19 +22,32 @@ const (
 )
 
 var (
-	GlobalSyslogService = NewSyslogWatcherService()
+	mu             sync.Mutex
+	gSyslogService *SyslogWatcherService
 )
+
+func GlobalSyslogService(config_obj *config_proto.Config) *SyslogWatcherService {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if gSyslogService == nil {
+		gSyslogService = NewSyslogWatcherService(config_obj)
+	}
+	return gSyslogService
+}
 
 // This service watches one or more event logs files and multiplexes
 // events to multiple readers.
 type SyslogWatcherService struct {
 	mu sync.Mutex
 
+	config_obj    *config_proto.Config
 	registrations map[string][]*Handle
 }
 
-func NewSyslogWatcherService() *SyslogWatcherService {
+func NewSyslogWatcherService(config_obj *config_proto.Config) *SyslogWatcherService {
 	return &SyslogWatcherService{
+		config_obj:    config_obj,
 		registrations: make(map[string][]*Handle),
 	}
 }
@@ -169,47 +184,61 @@ func (self *SyslogWatcherService) monitorOnce(
 		return cursor
 	}
 
+	// The file is suddenly smaller than before - this could mean the
+	// file was truncated.
+	if stat.Size() < cursor.file_size {
+		logger := logging.GetLogger(self.config_obj, &logging.ClientComponent)
+		logger.Info("File size (%v) is smaller than last know size (%v) - assuming file was truncated. Will start reading at the start again.",
+			stat.Size(), cursor.file_size)
+		cursor.last_line_offset = 0
+	}
+
+	cursor.file_size = stat.Size()
+
 	key := filename + accessor_name
 	handles, pres := self.registrations[key]
 	if !pres {
-		return &Cursor{}
+		return cursor
 	}
 
 	fd, err := accessor.Open(filename)
 	if err != nil {
-		return &Cursor{}
+		return cursor
 	}
 	defer fd.Close()
 
 	// File must be seekable
 	pos, err := fd.Seek(cursor.last_line_offset, 0)
 	if err != nil {
-		return &Cursor{}
+		return cursor
 	}
 
 	// File is truncated, start reading from the front again.
 	if cursor.last_line_offset != pos {
-		cursor = &Cursor{}
+		cursor.last_line_offset = 0
+		return cursor
 	}
 
 	buff := make([]byte, BUFFER_SIZE)
 	n, err := fd.Read(buff)
 	if err != nil && err != io.EOF {
-		return &Cursor{}
+		return cursor
 	}
 
 	buff = buff[:n]
+	offset := 0
 
 	// Read whole lines inside the buffer
-	for len(buff) > 0 {
-		new_lf := bytes.IndexByte(buff, '\n')
+	for len(buff)-offset > 0 {
+		new_lf := bytes.IndexByte(buff[offset:], '\n')
 		if new_lf > 0 {
 			new_handles := self.distributeLine(
-				string(buff[:new_lf]), filename, key, handles)
+				string(buff[offset:offset+new_lf]), filename, key, handles)
 
 			// No more listeners - we dont care any more.
 			if len(new_handles) == 0 {
 				delete(self.registrations, key)
+				cursor.last_line_offset += int64(new_lf) + 1
 				return cursor
 			}
 
@@ -219,20 +248,16 @@ func (self *SyslogWatcherService) monitorOnce(
 			handles = new_handles
 
 			cursor.last_line_offset += int64(new_lf) + 1
-			buff = buff[new_lf+1:]
+			offset += new_lf + 1
 
 		} else {
 			// The buffer does not contain lf at all - we
 			// skip the entire buffer and hope to get a
 			// line on the next read.
-			if cursor.last_line_offset+BUFFER_SIZE < cursor.file_size {
-				cursor.last_line_offset = cursor.file_size
-			}
-
+			cursor.last_line_offset += int64(len(buff) - offset)
 			return cursor
 		}
 	}
-
 	return cursor
 }
 
