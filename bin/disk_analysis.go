ee40c19afa007d62589de546395d77b3cc8b8a61
lucebac <bitbucket@lucebac.net>
2022-01-06 19:36:51 +0100

    redo command line args

--- bin/disk_analysis.go
+++ bin/disk_analysis.go
@@ -6,12 +6,8 @@ import (
 	config_proto "www.velocidex.com/golang/velociraptor/config/proto"
 )
 
-func applyDiskAnalysisSettings(config_obj *config_proto.Config) error {
-	config_obj.AnalysisTarget = *analysis_target_flag
-	config_obj.Device = *device_flag
-	config_obj.DeviceAccessor = *device_accessor_flag
-
-	if *device_flag != "" && *device_accessor_flag == "" {
+func verifyDiskAnalysisSettings(config_obj *config_proto.Config) error {
+	if config_obj.Device != "" && config_obj.DeviceAccessor == "" {
 		return errors.New("cannot use `device' flag without `device_accessor' flag")
 	}
 
