d536c86aa09a5425f160551a6ae0d6584cd0b4db
Mike Cohen <mike@velocidex.com>
2022-02-18 01:15:38 +1000

    Modified Windows.Forensics.Prefetch to use VQL binary parser
    Previously the prefetch parser was written in Go. This is inflexible
    however as we can not adjust the parser by customizing the artifacts.
    
    This PR re-implements the prefetch parser in pure VQL using the binary
    parser. It also adds support for all data structures described in
    https://github.com/libyal/libscca/blob/main/documentation/Windows%20Prefetch%20File%20(PF)%20format.asciidoc

---
+++ vql/parsers/lzxpress.go
@@ -0,0 +1,51 @@
+package parsers
+
+import (
+	"context"
+
+	"github.com/Velocidex/ordereddict"
+	prefetch "www.velocidex.com/golang/go-prefetch"
+	vql_subsystem "www.velocidex.com/golang/velociraptor/vql"
+	vfilter "www.velocidex.com/golang/vfilter"
+	"www.velocidex.com/golang/vfilter/arg_parser"
+)
+
+type LZXpressFunctionArgs struct {
+	Data string `vfilter:"required,field=data,doc=The lzxpress stream (bytes)"`
+}
+
+// The hash fuction calculates a hash of a file. It may be expensive
+// so we make it cancelllable.
+type LZXpressFunction struct{}
+
+func (self *LZXpressFunction) Call(ctx context.Context,
+	scope vfilter.Scope,
+	args *ordereddict.Dict) vfilter.Any {
+	arg := &LZXpressFunctionArgs{}
+	err := arg_parser.ExtractArgsWithContext(ctx, scope, args, arg)
+	if err != nil {
+		scope.Log("lzxpress_decompress: %v", err)
+		return vfilter.Null{}
+	}
+
+	decompressed, err := prefetch.LZXpressHuffmanDecompressWithFallback(
+		[]byte(arg.Data), len(arg.Data))
+	if err != nil {
+		scope.Log("lzxpress_decompress: %v", err)
+		return vfilter.Null{}
+	}
+
+	return decompressed
+}
+
+func (self LZXpressFunction) Info(scope vfilter.Scope, type_map *vfilter.TypeMap) *vfilter.FunctionInfo {
+	return &vfilter.FunctionInfo{
+		Name:    "lzxpress_decompress",
+		Doc:     "Decompress an lzxpress blob.",
+		ArgType: type_map.AddType(scope, &LZXpressFunctionArgs{}),
+	}
+}
+
+func init() {
+	vql_subsystem.RegisterFunction(&LZXpressFunction{})
+}
