303bd6752fcc40913fb2a12b0955dddd1e620fb4
Mike Cohen <mike@velocidex.com>
2022-03-01 23:46:20 +1000

    Release 0.6.3-1 update

--- constants/constants.go
+++ constants/constants.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	VERSION                    = "0.6.3"
+	VERSION                    = "0.6.3-1"
 	ENROLLMENT_WELL_KNOWN_FLOW = "E:Enrol"
 	MONITORING_WELL_KNOWN_FLOW = FLOW_PREFIX + "Monitoring"
 
