16b683bae79829ad28593d57208aa02d86354eb5
Mike Cohen <mike@velocidex.com>
2022-04-15 07:32:06 +1000

    Revert to server 2019

--- bin/golden.go
+++ bin/golden.go
@@ -35,6 +35,7 @@ import (
 	errors "github.com/pkg/errors"
 	"github.com/sergi/go-diff/diffmatchpatch"
 	"github.com/shirou/gopsutil/v3/process"
+	"www.velocidex.com/golang/velociraptor/actions"
 	actions_proto "www.velocidex.com/golang/velociraptor/actions/proto"
 	config_proto "www.velocidex.com/golang/velociraptor/config/proto"
 	"www.velocidex.com/golang/velociraptor/constants"
@@ -130,6 +131,12 @@ func makeCtxWithTimeout(duration int) (context.Context, func()) {
 				p = pprof.Lookup("mutex")
 				if p != nil {
 					_ = p.WriteTo(os.Stdout, 1)
+				}
+
+				// Write the recent queries.
+				fmt.Println("Recent Queries.")
+				for _, q := range actions.QueryLog.Get() {
+					fmt.Println(q)
 				}
 
 				os.Stdout.Close()
