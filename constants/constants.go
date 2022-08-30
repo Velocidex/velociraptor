08d3af2b87ec3399f992a5cf3e69b83947001f93
Mike Cohen <mike@velocidex.com>
2022-08-30 23:45:58 +1000

    Prepare for the 0.6.6-rc2 release

--- constants/constants.go
+++ constants/constants.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	VERSION                    = "0.6.6-rc1"
+	VERSION                    = "0.6.6-rc2"
 	ENROLLMENT_WELL_KNOWN_FLOW = "E:Enrol"
 	MONITORING_WELL_KNOWN_FLOW = FLOW_PREFIX + "Monitoring"
 
