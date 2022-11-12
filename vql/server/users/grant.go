13f3d38549628756916d4a3f8140296d0395cf29
Mike Cohen <scudette@users.noreply.github.com>
2022-11-12 21:00:18 +1000

    Update user delete VQL and grant (#2238)
    Previously user_delete() would delete from all orgs but this can lead
    to an accident when the orgs parameter is omitted. The new behavior is
    to delete from the current org when no orgs are specified.

--- vql/server/users/grant.go
+++ vql/server/users/grant.go
@@ -5,16 +5,19 @@ import (
 
 	"github.com/Velocidex/ordereddict"
 	acl_proto "www.velocidex.com/golang/velociraptor/acls/proto"
+	"www.velocidex.com/golang/velociraptor/json"
 	"www.velocidex.com/golang/velociraptor/users"
+	"www.velocidex.com/golang/velociraptor/utils"
 	vql_subsystem "www.velocidex.com/golang/velociraptor/vql"
 	"www.velocidex.com/golang/vfilter"
 	"www.velocidex.com/golang/vfilter/arg_parser"
 )
 
 type GrantFunctionArgs struct {
-	Username string   `vfilter:"required,field=user,doc=The user to create or update."`
-	Roles    []string `vfilter:"required,field=roles,doc=List of roles to give the user."`
-	OrgIds   []string `vfilter:"optional,field=orgs,doc=One or more org IDs to grant access to."`
+	Username string            `vfilter:"required,field=user,doc=The user to create or update."`
+	Roles    []string          `vfilter:"optional,field=roles,doc=List of roles to give the user."`
+	OrgIds   []string          `vfilter:"optional,field=orgs,doc=One or more org IDs to grant access to. If not specified we use current org"`
+	Policy   *ordereddict.Dict `vfilter:"optional,field=policy,doc=A dict of permissions to set (e.g. as obtained from the gui_users() function)."`
 }
 
 type GrantFunction struct{}
@@ -38,18 +41,32 @@ func (self GrantFunction) Call(
 		return vfilter.Null{}
 	}
 
-	orgs := users.LIST_ALL_ORGS
+	// If org ids not specified we use the current org id
+	orgs := arg.OrgIds
 	if len(arg.OrgIds) == 0 {
 		orgs = []string{org_config_obj.OrgId}
 	}
 
-	policy := &acl_proto.ApiClientACL{
-		Roles: arg.Roles,
+	policy := &acl_proto.ApiClientACL{}
+	if !utils.IsNil(arg.Policy) {
+		serialized, err := arg.Policy.MarshalJSON()
+		if err != nil {
+			scope.Log("user_grant: %s", err)
+			return vfilter.Null{}
+		}
+		err = json.Unmarshal(serialized, policy)
+		if err != nil {
+			scope.Log("user_grant: %s", err)
+			return vfilter.Null{}
+		}
+	} else if len(arg.Roles) == 0 {
+		scope.Log("user_grant: You must provide either roles or a policy object")
+		return vfilter.Null{}
 	}
+	policy.Roles = arg.Roles
 
 	principal := vql_subsystem.GetPrincipal(scope)
-	err = users.AddUserToOrg(ctx, users.UseExistingUser,
-		principal, arg.Username, orgs, policy)
+	err = users.GrantUserToOrg(ctx, principal, arg.Username, orgs, policy)
 	if err != nil {
 		scope.Log("user_grant: %s", err)
 		return vfilter.Null{}
