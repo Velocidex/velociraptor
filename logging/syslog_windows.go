8829b014c3371601973ecf5ac704eae0b0ebe366
Mike Cohen <mike@velocidex.com>
2023-04-13 03:19:20 +1000

    Skip syslog for windows servers

---
+++ logging/syslog_windows.go
@@ -0,0 +1,11 @@
+// +build windows
+
+package logging
+
+import config_proto "www.velocidex.com/golang/velociraptor/config/proto"
+
+// Syslog is not supported on Windows.
+func maybeAddRemoteSyslog(
+	config_obj *config_proto.Config, manager *LogManager) error {
+	return nil
+}
