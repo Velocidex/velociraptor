bf9ac6c19de275134ac47a986a66bafc7544295d
Mike Cohen <mike@velocidex.com>
2023-04-13 02:52:00 +1000

    Fixed tests

--- logging/logging_test.go
+++ logging/logging_test.go
@@ -1,6 +1,7 @@
 package logging_test
 
 import (
+	"context"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -10,8 +11,11 @@ import (
 
 	"github.com/Velocidex/ordereddict"
 	"github.com/sebdah/goldie"
+	"github.com/stretchr/testify/suite"
 	"www.velocidex.com/golang/velociraptor/config"
+	"www.velocidex.com/golang/velociraptor/file_store/test_utils"
 	"www.velocidex.com/golang/velociraptor/logging"
+	"www.velocidex.com/golang/velociraptor/services"
 	"www.velocidex.com/golang/velociraptor/utils"
 	"www.velocidex.com/golang/velociraptor/vtesting/assert"
 )
@@ -21,7 +25,13 @@ type TestStruct struct {
 	Message string
 }
 
-func TestAuditLog(t *testing.T) {
+type LoggingTestSuite struct {
+	test_utils.TestSuite
+}
+
+func (self *LoggingTestSuite) TestAuditLog() {
+	t := self.T()
+
 	dir, err := ioutil.TempDir("", "file_store_test")
 	assert.NoError(t, err)
 	defer os.RemoveAll(dir)
@@ -37,12 +47,13 @@ func TestAuditLog(t *testing.T) {
 	err = logging.InitLogging(config_obj)
 	assert.NoError(t, err)
 
-	logging.LogAudit(config_obj, "Principal", "SomeOperation",
+	services.LogAudit(context.Background(),
+		config_obj, "Principal", "SomeOperation",
 		ordereddict.NewDict().
 			Set("SomeField", 1).
-			Set("NestedField", ordereddict.NewDict()).
-			Set("Field1", 1).
-			Set("Field2", 3).
+			Set("NestedField", ordereddict.NewDict().
+				Set("Field1", 1).
+				Set("Field2", 3)).
 			Set("StructField", &TestStruct{
 				Int1:    54,
 				Message: "Hello",
@@ -59,4 +70,8 @@ func TestAuditLog(t *testing.T) {
 	assert.NoError(t, err)
 
 	goldie.Assert(t, "TestAuditLog", data)
+}
+
+func TestLogging(t *testing.T) {
+	suite.Run(t, &LoggingTestSuite{})
 }
