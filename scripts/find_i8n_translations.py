cc6da8f3a129172b680892612062ce3513ca60b9
MD SHAHIL <96812916+Shahil093@users.noreply.github.com>
2023-06-06 10:47:01 +0530

    Update find_i8n_translations.py

--- scripts/find_i8n_translations.py
+++ scripts/find_i8n_translations.py
@@ -1,9 +1,3 @@
-#!/usr/bin/python3
-
-"""Finds all untranslated translations in the source tree.
-
-"""
-
 import argparse
 import re
 import os
@@ -38,18 +32,23 @@ def Decode(k):
         print("While decoding %s" % k)
         raise e
 
-def ProcessFile(filename):
+def ProcessFile(path):
     translations = dict()
 
-    with open(filename) as fd:
-        for line in fd:
-            m = key_regex.search(line)
-            if m:
-                translations[m.group(2)] = True
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if not name.endswith(".jsx"):
+                continue
+            filename = os.path.join(root, name)
+            with open(filename) as fd:
+                for line in fd:
+                    m = key_regex.search(line)
+                    if m:
+                        translations[m.group(2)] = True
 
     discovered = DiscoverTranslations('./gui/velociraptor/src/components/')
 
-    with open(os.path.splitext(filename)[0] + ".json") as fd:
+    with open(os.path.splitext(path)[0] + ".json") as fd:
         encoded_existing = json.loads(fd.read())
         existing = dict()
         existing_translations = dict()
@@ -60,15 +59,15 @@ def ProcessFile(filename):
     # The automated translations
     automated = dict()
     for k in discovered:
-        if not k in translations and not k in existing:
+        if k not in translations and k not in existing:
             automated[Encode(k)] = k
 
-    outfile = os.path.splitext(filename)[0] + "_new.json"
+    outfile = os.path.splitext(path)[0] + "_new.json"
     with open(outfile, "w") as outfd:
         outfd.write(json.dumps(automated, sort_keys=True, indent=4))
         print("Wrote json file %s with %d entries" % (outfile, len(automated)))
 
-    outfile = os.path.splitext(filename)[0] + "_automated.json"
+    outfile = os.path.splitext(path)[0] + "_automated.json"
     with open(outfile, "w") as outfd:
         outfd.write(json.dumps(existing_translations, sort_keys=True, indent=4))
         print("Wrote automated json file %s with %d entries" % (outfile, len(existing_translations)))
@@ -76,7 +75,7 @@ def ProcessFile(filename):
 
 if __name__ == "__main__":
     argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument("language_file", help="Language file to open")
+    argument_parser.add_argument("path", help="Directory path containing .jsx files")
 
     args = argument_parser.parse_args()
-    ProcessFile(args.language_file)
+    ProcessFile(args.path)
