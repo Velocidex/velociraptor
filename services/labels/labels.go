aa529bf18c5614b3b25fa88ec7bbdb6bbd4082b7
Pedro Gonzalez <plgonzalezrx8@users.noreply.github.com>
2023-06-22 09:23:47 -0400

    Trimmed Spaces around labels in labels.go
    Trimmed Spaces around labels

--- services/labels/labels.go
+++ services/labels/labels.go
@@ -201,7 +201,7 @@ func (self *Labeler) SetClientLabel(
 	self.mu.Lock()
 	defer self.mu.Unlock()
 
-	checked_label := strings.ToLower(new_label)
+	checked_label := strings.ToLower(strings.TrimSpace(new_label))
 	cached, err := self.getRecord(config_obj, client_id)
 	if err != nil {
 		return err
