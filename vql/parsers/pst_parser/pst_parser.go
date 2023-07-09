f9c454617276a54ceeb63297c37db624ee63db4e
SikhaPuthanveedu <sikhamohanam@gmail.com>
2023-07-09 00:19:41 -0400

    code and test file modified after review

--- vql/parsers/pst_parser/pst_parser.go
+++ vql/parsers/pst_parser/pst_parser.go
@@ -33,7 +33,7 @@ func (self *PSTParser) Call(ctx context.Context, scope vfilter.Scope, args *orde
 		arg := &PSTParserArgs{}
 		err := vfilter.ExtractArgs(scope, args, arg)
 		if err != nil {
-			scope.Log("pst_parser: %v", err)
+			scope.Log("pst_parser: ExtractArgs %v", err)
 			return
 		}
 
@@ -47,7 +47,7 @@ func (self *PSTParser) Call(ctx context.Context, scope vfilter.Scope, args *orde
 
 		pstFile, err := pst.New(reader)
 		if err != nil {
-			scope.Log("pst_parser: %v", err)
+			scope.Log("pst_parser:pst.New(reader) %v", err)
 			return
 		}
 
@@ -55,7 +55,7 @@ func (self *PSTParser) Call(ctx context.Context, scope vfilter.Scope, args *orde
 			pstFile.Cleanup()
 
 			if errClosing := reader.Close(); errClosing != nil {
-				scope.Log("pst_parser: %v", errClosing)
+				scope.Log("pst_parser:reader.Close() %v", errClosing)
 			}
 		}()
 
@@ -89,9 +89,7 @@ func (self *PSTParser) Call(ctx context.Context, scope vfilter.Scope, args *orde
 				// Folder has no messages.
 				return nil
 			} else if err != nil {
-				scope.Log("WalkFolders error")
-				scope.Log("Walking folder: %s\n", folder.Name)
-				return err
+				scope.Log("Walking folder error: %s\n", folder.Name)
 			}
 
 			for messageIterator.Next() {
@@ -99,14 +97,19 @@ func (self *PSTParser) Call(ctx context.Context, scope vfilter.Scope, args *orde
 
 				// Process the message and send it to the output channel
 				output := ordereddict.NewDict()
-				output.Set("Sender", message.Properties.(*properties.Message).GetSenderEmailAddress())
-				output.Set("Receiver", message.Properties.(*properties.Message).GetReceivedByEmailAddress())
-				output.Set("Subject", message.Properties.(*properties.Message).GetSubject())
-				output.Set("Message", message.Properties.(*properties.Message).GetBody())
-				output.Set("Body", message.Properties.(*properties.Message).String())
+				message_props, ok := message.Properties.(*properties.Message)
+				if !ok {
+					continue
+				}
+
+				output.Set("Sender", message_props.GetSenderEmailAddress())
+				output.Set("Receiver", message_props.GetReceivedByEmailAddress())
+				output.Set("Subject", message_props.GetSubject())
+				output.Set("Message", message_props.GetBody())
+				output.Set("Body", message_props.String())
 
 				// Convert the int64 timestamp (in milliseconds) to a time.Time value
-				deliveryTime := message.Properties.(*properties.Message).GetMessageDeliveryTime() / 1e9
+				deliveryTime := message_props.GetMessageDeliveryTime() / 1e9
 				deliveryTimeValue := time.Unix(deliveryTime, 0).UTC()
 
 				// Format the date and time in UTC
@@ -124,7 +127,7 @@ func (self *PSTParser) Call(ctx context.Context, scope vfilter.Scope, args *orde
 					outputChan <- output
 					continue
 				} else if err != nil {
-					return err
+					scope.Log("ErrAttachmentsNotFound")
 				}
 
 				// Iterate through attachments.
@@ -136,50 +139,55 @@ func (self *PSTParser) Call(ctx context.Context, scope vfilter.Scope, args *orde
 					if attachment.GetAttachLongFilename() != "" {
 						attachmentNameId = fmt.Sprintf("%d-%s", attachment.Identifier, attachment.GetAttachLongFilename())
 						attachmentName = append(attachmentName, attachmentNameId)
-						// Set attachment name and Id to the output channel
-						output.Set("AttachmentId", attachment.Identifier)
-						output.Set("Attachments", attachmentName)
 
 					} else {
+						attachmentNameId = fmt.Sprintf("%d-%s", attachment.Identifier, "")
+						attachmentName = append(attachmentName, attachmentNameId)
 						scope.Log("attachments/UNKNOWN_%d", attachment.Identifier)
 					}
+					// Set attachment name and Id to the output channel
+					output.Set("AttachmentId", attachment.Identifier)
+					output.Set("Attachments", attachmentName)
 
-					// Save to attachments folder
-					var attachmentOutputPath string
+					// Save to attachments folder only if FolderPath is specified
+					if arg.FolderPath != "" {
+						var attachmentOutputPath string
 
-					if attachment.GetAttachLongFilename() != "" {
-						attachmentOutputPath = fmt.Sprintf(arg.FolderPath+"/%d-%s", attachment.Identifier, attachment.GetAttachLongFilename())
-					} else {
-						attachmentOutputPath = fmt.Sprintf("attachments/UNKNOWN_%d", attachment.Identifier)
-						scope.Log("attachments/UNKNOWN_%d", attachment.Identifier)
+						if attachment.GetAttachLongFilename() != "" {
+							attachmentOutputPath = fmt.Sprintf(arg.FolderPath+"/%d-%s", attachment.Identifier, attachment.GetAttachLongFilename())
+						} else {
+							attachmentOutputPath = fmt.Sprintf("attachments/UNKNOWN_%d", attachment.Identifier)
+						}
+
+						attachmentOutput, err := os.Create(attachmentOutputPath)
+						if err != nil {
+							scope.Log("Error while creating Attchment_%d\n", attachment.Identifier)
+							continue
+						}
+
+						if _, err := attachment.WriteTo(attachmentOutput); err != nil {
+							scope.Log("Error while writing Attchment_%d\n", attachment.Identifier)
+						}
+
+						if err := attachmentOutput.Close(); err != nil {
+							scope.Log("Error while closing attachmentOutput_%d\n", attachment.Identifier)
+						}
+						if attachmentIterator.Err() != nil {
+							scope.Log("Error while iterating atachments\n")
+							continue
+						}
 					}
-
-					attachmentOutput, err := os.Create(attachmentOutputPath)
-					if err != nil {
-						return err
-					}
-
-					if _, err := attachment.WriteTo(attachmentOutput); err != nil {
-						return err
-					}
-
-					if err := attachmentOutput.Close(); err != nil {
-						return err
-					}
-				}
-				if attachmentIterator.Err() != nil {
-					return attachmentIterator.Err()
 				}
 				outputChan <- output
 			}
 
 			if messageIterator.Err() != nil {
-				return messageIterator.Err()
+				scope.Log("Error while iterating messages\n")
 			}
 
 			return nil
 		}); err != nil {
-			scope.Log("pst_parser: %v", err)
+			scope.Log("pst_parser_WalkFolders error: %v", err)
 		}
 	}()
 
