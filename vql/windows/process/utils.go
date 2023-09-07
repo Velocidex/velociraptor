8d3e20e6489085be91d83495a000ae8f8cca5bf8
Mike Cohen <mike@velocidex.com>
2023-09-08 02:50:57 +1000

    Report process names as well as pid for errors
    In Windows many processes are protected so we can not access
    them. Previously Velociraptor only reported the pid but this PR also
    reports the process name for inaccessible processes

---
+++ vql/windows/process/utils.go
@@ -0,0 +1,24 @@
+package process
+
+import (
+	"context"
+	"fmt"
+
+	"www.velocidex.com/golang/velociraptor/vql/tools/process"
+	"www.velocidex.com/golang/vfilter"
+)
+
+func GetProcessContext(
+	ctx context.Context, scope vfilter.Scope, pid uint64) string {
+	tracker := process.GetGlobalTracker()
+	record, ok := tracker.Get(ctx, scope, fmt.Sprintf("%v", pid))
+	if ok {
+		name, _ := record.Data.GetString("Name")
+		if name == "" {
+			name = "unknown"
+		}
+		return fmt.Sprintf(" %v (%v) ", record.Id, name)
+	}
+
+	return fmt.Sprintf(" %v ", pid)
+}
