02e45a2aa9d40f8db1376ad107d1b966a6e5601d
Mike Cohen <mike@velocidex.com>
2023-09-27 07:07:42 +1000

    Convert sysinfo to a function

--- vql/linux/sysinfo.go
+++ vql/linux/sysinfo.go
@@ -15,49 +15,40 @@ import (
 	"www.velocidex.com/golang/vfilter"
 )
 
-type LinuxSysinfoPlugin struct{}
+type LinuxSysinfoFunction struct{}
 
-func (self LinuxSysinfoPlugin) Info(scope vfilter.Scope, type_map *vfilter.TypeMap) *vfilter.PluginInfo {
-	return &vfilter.PluginInfo{
-		Name:     "linux_sysinfo",
+func (self LinuxSysinfoFunction) Info(scope vfilter.Scope, type_map *vfilter.TypeMap) *vfilter.FunctionInfo {
+	return &vfilter.FunctionInfo{
+		Name:     "sysinfo",
 		Doc:      "Collect system information on Linux clients",
 		Metadata: vql.VQLMetadata().Permissions(acls.MACHINE_STATE).Build(),
 	}
 }
 
-func (self LinuxSysinfoPlugin) Call(
-	ctx context.Context, scope vfilter.Scope,
-	args *ordereddict.Dict) <-chan vfilter.Row {
-	output_chan := make(chan vfilter.Row)
+func (self LinuxSysinfoFunction) Call(ctx context.Context, scope vfilter.Scope,
+	args *ordereddict.Dict) vfilter.Any {
 
-	go func() {
-		defer close(output_chan)
+	err := vql_subsystem.CheckAccess(scope, acls.MACHINE_STATE)
+	if err != nil {
+		scope.Log("sysinfo: %s", err)
+		return vfilter.Null{}
+	}
 
-		err := vql_subsystem.CheckAccess(scope, acls.MACHINE_STATE)
-		if err != nil {
-			scope.Log("linux_sysinfo: %s", err)
-			return
-		}
+	in := &syscall.Sysinfo_t{}
+	err = syscall.Sysinfo(in)
+	if err != nil {
+		scope.Log("sysinfo: sysinfo() failed: %s", err)
+		return vfilter.Null{}
+	}
 
-		in := &syscall.Sysinfo_t{}
-		err = syscall.Sysinfo(in)
-		if err != nil {
-			scope.Log("linux_sysinfo: sysinfo() failed: %s", err)
-			return
-		}
-
-		row := ordereddict.NewDict().
-			Set("TotalPhysicalMemory", uint64(in.Totalram)*uint64(in.Unit)).
-			Set("TotalFreeMemory", uint64(in.Freeram)*uint64(in.Unit)).
-			Set("TotalSharedMemory", uint64(in.Sharedram)*uint64(in.Unit)).
-			Set("TotalSwap", uint64(in.Totalswap)*uint64(in.Unit)).
-			Set("FreeSwap", uint64(in.Freeswap)*uint64(in.Unit))
-		output_chan <- row
-	}()
-
-	return output_chan
+	return ordereddict.NewDict().
+		Set("TotalPhysicalMemory", uint64(in.Totalram)*uint64(in.Unit)).
+		Set("TotalFreeMemory", uint64(in.Freeram)*uint64(in.Unit)).
+		Set("TotalSharedMemory", uint64(in.Sharedram)*uint64(in.Unit)).
+		Set("TotalSwap", uint64(in.Totalswap)*uint64(in.Unit)).
+		Set("FreeSwap", uint64(in.Freeswap)*uint64(in.Unit))
 }
 
 func init() {
-	vql_subsystem.RegisterPlugin(&LinuxSysinfoPlugin{})
+	vql_subsystem.RegisterFunction(&LinuxSysinfoFunction{})
 }
