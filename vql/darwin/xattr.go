e8961b7e65211717bd040551f0184e6b665e2c49
bmcdermott <bmcdermott@rapid7.com>
2024-02-12 11:50:38 +1100

    Updated tests, added a golden test.

--- vql/darwin/xattr.go
+++ vql/darwin/xattr.go
@@ -28,7 +28,6 @@ func (self XAttrFunction) Call(
 	scope vfilter.Scope,
 	args *ordereddict.Dict) vfilter.Any {
 	defer vql_subsystem.CheckForPanic(scope, "xattr")
-	attr := map[string]string{}
 	arg := &XAttrArgs{}
 	err := arg_parser.ExtractArgsWithContext(ctx, scope, args, arg)
 	if err != nil {
@@ -42,49 +41,43 @@ func (self XAttrFunction) Call(
 		return nil
 	}
 
-	filename, err := accessors.GetUnderlyingAPIFilename(arg.Accessor, scope, arg.Filename)
+	filename, err := accessors.GetUnderlyingAPIFilename(
+		arg.Accessor, scope, arg.Filename)
 	if err != nil {
-		scope.Log("xattr: Failed to get underlying filename for %s: %s", arg.Filename.String(), err)
+		scope.Log("xattr: Failed to get underlying filename for %s: %s",
+			arg.Filename.String(), err)
 	}
 
 	if len(arg.Attributes) > 0 {
-		attr = self.getAttributeValues(scope, arg.Attributes, filename)
+		return self.getAttributeValues(scope, arg.Attributes, filename)
 	} else {
 		attributes, err := xattr.List(filename)
 		if err != nil {
-			scope.Log("xattr: Failed to list attributes for filename %s: %s", filename, err)
+			scope.Log("xattr: Failed to list attributes for filename %s: %s",
+				filename, err)
 			return vfilter.Null{}
 		}
 
-		attr = self.getAttributeValues(scope, attributes, filename)
+		return self.getAttributeValues(scope, attributes, filename)
 	}
-
-	if attr == nil {
-		return vfilter.Null{}
-	}
-
-	return ordereddict.NewDict().
-		Set("filename", filename).
-		Set("attribute", attr)
 }
 
-func (self *XAttrFunction) getAttributeValues(scope vfilter.Scope, Attributes []string, Filename string) map[string]string {
-	ret := map[string]string{}
+func (self *XAttrFunction) getAttributeValues(
+	scope vfilter.Scope, Attributes []string,
+	Filename string) *ordereddict.Dict {
+	ret := ordereddict.NewDict()
 	for _, attr := range Attributes {
 		value, err := xattr.Get(Filename, attr)
 		if err != nil {
 			continue
 		}
-		ret[attr] = string(value)
-	}
-	if len(ret) == 0 {
-		scope.Log("xattr: Failed to get attribute values for filename %s", Filename)
-		return nil
+		ret.Set(attr, value)
 	}
 	return ret
 }
 
-func (self XAttrFunction) Info(scope vfilter.Scope, type_map *vfilter.TypeMap) *vfilter.FunctionInfo {
+func (self XAttrFunction) Info(
+	scope vfilter.Scope, type_map *vfilter.TypeMap) *vfilter.FunctionInfo {
 	return &vfilter.FunctionInfo{
 		Name:     "xattr",
 		Doc:      "query a file for the specified extended attribute",
--- vql/darwin/xattr_test.go
+++ vql/darwin/xattr_test.go
@@ -1,21 +1,24 @@
 package darwin
 
 import (
-	"context"
+	"fmt"
 	"os"
 	"testing"
 
 	"github.com/Velocidex/ordereddict"
 	"github.com/ivaxer/go-xattr"
+	"github.com/sebdah/goldie"
 	"github.com/stretchr/testify/suite"
 	_ "www.velocidex.com/golang/velociraptor/accessors/file"
 	"www.velocidex.com/golang/velociraptor/file_store/test_utils"
+	"www.velocidex.com/golang/velociraptor/json"
 	"www.velocidex.com/golang/velociraptor/logging"
 	"www.velocidex.com/golang/velociraptor/services"
 	"www.velocidex.com/golang/velociraptor/vql/acl_managers"
 	"www.velocidex.com/golang/velociraptor/vtesting/assert"
-	"www.velocidex.com/golang/vfilter"
 )
+
+var ()
 
 type XattrTestSuite struct {
 	test_utils.TestSuite
@@ -29,7 +32,9 @@ func (self *XattrTestSuite) TestXAttr() {
 	defer os.Remove(filepath)
 
 	attr1 := "vr.test"
-	attr2 := "vr.test"
+	attr2 := "vr.test2"
+	value := "test.value"
+
 	testcases := []struct {
 		name string
 		attr []string
@@ -62,18 +67,15 @@ func (self *XattrTestSuite) TestXAttr() {
 		},
 	}
 
-	xattr.Set(filepath, attr1, []byte("test-value"))
-	xattr.Set(filepath, attr2, []byte("test-value"))
-
-	self.Ctx = context.Background()
-	sub_ctx, cancel := context.WithCancel(self.Ctx)
-	defer cancel()
+	xattr.Set(filepath, attr1, []byte(value))
+	xattr.Set(filepath, attr2, []byte(value))
 
 	builder := services.ScopeBuilder{
 		Config:     self.ConfigObj,
 		ACLManager: acl_managers.NullACLManager{},
-		Logger:     logging.NewPlainLogger(self.ConfigObj, &logging.FrontendComponent),
-		Env:        ordereddict.NewDict(),
+		Logger: logging.NewPlainLogger(self.ConfigObj,
+			&logging.FrontendComponent),
+		Env: ordereddict.NewDict(),
 	}
 
 	manager, err := services.GetRepositoryManager(self.ConfigObj)
@@ -82,11 +84,16 @@ func (self *XattrTestSuite) TestXAttr() {
 	scope := manager.BuildScope(builder)
 	defer scope.Close()
 
+	ret := ordereddict.NewDict()
 	for i, test := range testcases {
-		defer scope.Close()
-		ret := XAttrFunction{}.Call(sub_ctx, scope, ordereddict.NewDict().Set("filename", test.name).Set("attribute", test.attr))
-		assert.Equal(self.T(), test.pass, (ret != vfilter.Null{}), "These two values should be the same. Test %d", i)
+		ret.Set(fmt.Sprintf("Case #%d", i), XAttrFunction{}.Call(
+			self.Ctx, scope,
+			ordereddict.NewDict().
+				Set("filename", test.name).
+				Set("attribute", test.attr)))
+
 	}
+	goldie.Assert(self.T(), "xattrReturnCheck", json.MustMarshalIndent(ret))
 }
 
 func TestXAttrFunction(t *testing.T) {
