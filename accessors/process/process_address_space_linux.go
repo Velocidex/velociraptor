20b2a5120c1819bcc9013b2ab99b5b0d024ed92f
Mike Cohen <scudette@gmail.com>
2024-02-28 13:03:38 +1000

    Added a darwin process accessor.
    This allows proc_yara() to work on MacOS. Also added vad() plugin for macos.
    
    NOTE: for this to work the binary needs to get a number of entitlements:
    
    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
    <plist version="1.0">
    <dict>
            <key>com.apple.springboard.debugapplications</key>
            <true/>
            <key>get-task-allow</key>
            <true/>
            <key>proc_info-allow</key>
            <true/>
            <key>task_for_pid-allow</key>
            <true/>
            <key>run-unsigned-code</key>
            <true/>
    </dict>
    </plist>

---
+++ accessors/process/process_address_space_linux.go
@@ -0,0 +1,102 @@
+//go:build linux
+// +build linux
+
+package process
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+
+	"www.velocidex.com/golang/velociraptor/accessors"
+	"www.velocidex.com/golang/velociraptor/uploads"
+)
+
+func (self *ProcessAccessor) OpenWithOSPath(
+	path *accessors.OSPath) (accessors.ReadSeekCloser, error) {
+	if len(path.Components) == 0 {
+		return nil, errors.New("Unable to list all processes, use the pslist() plugin.")
+	}
+
+	pid, err := strconv.ParseUint(path.Components[0], 0, 64)
+	if err != nil {
+		return nil, errors.New("First directory path must be a process.")
+	}
+
+	// Open the device file for the process
+	fd, err := os.Open(fmt.Sprintf("/proc/%d/mem", pid))
+	if err != nil {
+		return nil, err
+	}
+
+	// Open the process and enumerate its ranges
+	ranges, err := GetVads(pid)
+	if err != nil {
+		return nil, err
+	}
+	result := &ProcessReader{
+		pid:    pid,
+		handle: fd,
+	}
+
+	for _, r := range ranges {
+		result.ranges = append(result.ranges, r)
+	}
+
+	return result, nil
+}
+
+var (
+	maps_regexp = regexp.MustCompile("(?P<Start>^[^-]+)-(?P<End>[^\\s]+)\\s+(?P<Perm>[^\\s]+)\\s+(?P<Size>[^\\s]+)\\s+[^\\s]+\\s+(?P<PermInt>[^\\s]+)\\s+(?P<Filename>.+?)(?P<Deleted> \\(deleted\\))?$")
+)
+
+func GetVads(pid uint64) ([]*uploads.Range, error) {
+	maps_fd, err := os.Open(fmt.Sprintf("/proc/%d/maps", pid))
+	if err != nil {
+		return nil, err
+	}
+	defer maps_fd.Close()
+
+	var result []*uploads.Range
+
+	scanner := bufio.NewScanner(maps_fd)
+	for scanner.Scan() {
+		hits := maps_regexp.FindStringSubmatch(scanner.Text())
+		if len(hits) > 0 {
+			protection := hits[3]
+			// Only include readable ranges.
+			if len(protection) < 2 || protection[0] != 'r' {
+				continue
+			}
+
+			start, err := strconv.ParseInt(hits[1], 16, 64)
+			if err != nil {
+				continue
+			}
+
+			end, err := strconv.ParseInt(hits[2], 16, 64)
+			if err != nil {
+				continue
+			}
+
+			// We can not read kernel memory
+			if start < 0 || end < 0 {
+				continue
+			}
+
+			result = append(result, &uploads.Range{
+				Offset: start, Length: end - start,
+			})
+		}
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
---
+++ vql/darwin/vad.go
@@ -0,0 +1,177 @@
+//go:build darwin && cgo
+// +build darwin,cgo
+
+package darwin
+
+import (
+	"context"
+	"fmt"
+	"unsafe"
+
+	"github.com/Velocidex/ordereddict"
+	vql_subsystem "www.velocidex.com/golang/velociraptor/vql"
+	"www.velocidex.com/golang/vfilter"
+	"www.velocidex.com/golang/vfilter/arg_parser"
+)
+
+/*
+#include <stdlib.h>
+#include <mach/mach_traps.h>
+#include <mach/mach.h>
+#include <mach/mach_vm.h>
+#include <mach/vm_region.h>
+#include <mach/vm_statistics.h>
+#include <libproc.h>
+
+mach_port_t   get_task_self () ;
+*/
+import "C"
+
+const (
+	// https://opensource.apple.com/source/xnu/xnu-792/osfmk/mach/vm_region.h.auto.html
+	VM_REGION_BASIC_INFO          = 10
+	VM_REGION_BASIC_INFO_COUNT_64 = 9
+
+	VM_PROT_READ    = 1
+	VM_PROT_WRITE   = 2
+	VM_PROT_EXECUTE = 4
+
+	MAX_PATH = 1024
+)
+
+type VMemInfo struct {
+	Address       uint64
+	Size          uint64
+	MappingName   string
+	State         string
+	Type          string
+	Protection    string
+	ProtectionMsg string
+	ProtectionRaw uint32
+}
+
+type PidArgs struct {
+	Pid int64 `vfilter:"required,field=pid,doc=The PID to dump out."`
+}
+
+type VADPlugin struct{}
+
+func (self VADPlugin) Call(
+	ctx context.Context,
+	scope vfilter.Scope,
+	args *ordereddict.Dict) <-chan vfilter.Row {
+	output_chan := make(chan vfilter.Row)
+	arg := &PidArgs{}
+
+	go func() {
+		defer close(output_chan)
+		defer vql_subsystem.CheckForPanic(scope, "vad")
+
+		err := arg_parser.ExtractArgsWithContext(ctx, scope, args, arg)
+		if err != nil {
+			scope.Log("vad: %s", err.Error())
+			return
+		}
+
+		vads, err := GetVads(arg.Pid)
+		if err != nil {
+			scope.Log("vad: %s", err.Error())
+			return
+		}
+
+		for _, vad := range vads {
+			select {
+			case <-ctx.Done():
+				return
+			case output_chan <- vad:
+			}
+		}
+	}()
+
+	return output_chan
+}
+
+func (self VADPlugin) Info(scope vfilter.Scope, type_map *vfilter.TypeMap) *vfilter.PluginInfo {
+	return &vfilter.PluginInfo{
+		Name:    "vad",
+		Doc:     "Enumerate process memory regions.",
+		ArgType: type_map.AddType(scope, &PidArgs{}),
+	}
+}
+
+func GetVads(pid int64) (res []*VMemInfo, err error) {
+	var task C.task_t
+
+	kr := C.task_for_pid(C.get_task_self(), C.int(pid), &task)
+	if kr != 0 {
+		return nil, fmt.Errorf("process: Can not open pid %v: %v", pid, kr)
+	}
+	defer func() {
+		C.mach_port_deallocate(C.get_task_self(), task)
+	}()
+
+	var address C.vm_address_t
+	var info_count C.mach_msg_type_number_t = VM_REGION_BASIC_INFO_COUNT_64
+	var object C.mach_port_t
+	var info C.vm_region_basic_info_data_t
+	var size C.vm_size_t
+
+	// Iterate through the address space getting all the regions.
+	for {
+
+		kr := C.vm_region_64(task, &address,
+			&size, VM_REGION_BASIC_INFO,
+			(*C.int)(unsafe.Pointer(&info)), &info_count, &object)
+		if kr != 0 {
+			break
+		}
+
+		// This only seems to work for some mappings.
+		buffer := (*C.char)(C.malloc(C.size_t(MAX_PATH)))
+		defer C.free(unsafe.Pointer(buffer))
+
+		buf_size := C.uint32_t(MAX_PATH)
+
+		C.proc_regionfilename((C.int)(pid), (C.uint64_t)(address),
+			unsafe.Pointer(buffer), buf_size)
+		res = append(res, &VMemInfo{
+			Address:       uint64(address),
+			Size:          uint64(size),
+			Protection:    getProtection(uint32(info.protection)),
+			ProtectionRaw: uint32(info.protection),
+			MappingName:   C.GoString(buffer),
+		})
+		address += size
+		size = 0
+	}
+
+	return res, nil
+}
+
+func getProtection(p uint32) string {
+	result := ""
+
+	if p&VM_PROT_EXECUTE > 0 {
+		result += "x"
+	} else {
+		result += "-"
+	}
+
+	if p&VM_PROT_WRITE > 0 {
+		result += "w"
+	} else {
+		result += "-"
+	}
+
+	if p&VM_PROT_READ > 0 {
+		result += "r"
+	} else {
+		result += "-"
+
+	}
+	return result
+}
+
+func init() {
+	vql_subsystem.RegisterPlugin(&VADPlugin{})
+}
