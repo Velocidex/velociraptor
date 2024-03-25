b0163cac83f48be4ef7eed5f4fe2b52ba0d63398
Mike Cohen <mike@velocidex.com>
2024-03-25 16:54:44 +1000

    Implemented an eval() function

---
+++ vql/functions/eval.go
@@ -0,0 +1,41 @@
+package functions
+
+import (
+	"context"
+
+	"github.com/Velocidex/ordereddict"
+	vql_subsystem "www.velocidex.com/golang/velociraptor/vql"
+	"www.velocidex.com/golang/vfilter"
+	"www.velocidex.com/golang/vfilter/arg_parser"
+)
+
+type EvalFunctionArg struct {
+	Func *vfilter.Lambda `vfilter:"required,field=func,doc=Lambda function to evaluate e.g. x=>1+1 where x will be the current scope."`
+}
+
+type EvalFunction struct{}
+
+func (self EvalFunction) Info(scope vfilter.Scope, type_map *vfilter.TypeMap) *vfilter.FunctionInfo {
+	return &vfilter.FunctionInfo{
+		Name:    "eval",
+		Doc:     "Evaluate a vql lambda function on the current scope.",
+		ArgType: type_map.AddType(scope, EvalFunctionArg{}),
+	}
+}
+
+func (self EvalFunction) Call(ctx context.Context, scope vfilter.Scope,
+	args *ordereddict.Dict) vfilter.Any {
+	arg := &EvalFunctionArg{}
+	err := arg_parser.ExtractArgsWithContext(ctx, scope, args, arg)
+	if err != nil {
+		scope.Log("eval: %s", err.Error())
+		return vfilter.Null{}
+	}
+
+	// Evaluate the lambda on the current scope.
+	return arg.Func.Reduce(ctx, scope, []vfilter.Any{scope})
+}
+
+func init() {
+	vql_subsystem.RegisterFunction(&EvalFunction{})
+}
