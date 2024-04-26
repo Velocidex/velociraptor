343bdffc1c18d8d4ba8818e8bb21b05a5c5397e5
predictiple <predictiple@users.noreply.github.com>
2024-04-26 18:55:35 +0200

    Update to CuratedSigma artifact and some GUI improvements (#3455)
    - CuratedSigma artifact can now import multiple sources
    - Improved the react-select component's appearance in all themes. Change
    also to it's behaviour so that it closes after each selection because if
    it stays open then it overlays the parameter control below it, which is
    particularly confusing when only 1 item remains to be selected in the
    list.
    
    ---------
    
    Co-authored-by: Mike Cohen <mike@velocidex.com>

--- services/launcher/compiler.go
+++ services/launcher/compiler.go
@@ -139,7 +139,7 @@ LET %v <= if(
 					escaped_name, escaped_name, escaped_name, escaped_name),
 			})
 
-		case "json_array", "regex_array":
+		case "json_array", "regex_array", "multichoice":
 			result.Query = append(result.Query, &actions_proto.VQLRequest{
 				VQL: fmt.Sprintf(`
 LET %v <= if(
