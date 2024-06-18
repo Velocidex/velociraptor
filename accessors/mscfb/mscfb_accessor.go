287691e1ea333b6ddb15b799ed80e9398974e891
Mike Cohen <mike@velocidex.com>
2024-06-18 19:18:27 +1000

    Added mscfb (OLE) accessor and Jumplist artifact
    Parses the Automatic Destinations files.

---
+++ accessors/mscfb/mscfb_accessor.go
@@ -0,0 +1,269 @@
+package mscfb
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"runtime/debug"
+	"time"
+
+	"github.com/Velocidex/go-mscfb/parser"
+	"github.com/Velocidex/ordereddict"
+	"www.velocidex.com/golang/velociraptor/accessors"
+	"www.velocidex.com/golang/velociraptor/json"
+	"www.velocidex.com/golang/vfilter"
+)
+
+type MscfbFileInfo struct {
+	entry      *parser.DirectoryEntry
+	_full_path *accessors.OSPath
+}
+
+func (self *MscfbFileInfo) Name() string {
+	return self.entry.Name
+}
+
+func (self *MscfbFileInfo) UniqueName() string {
+	return self._full_path.String()
+}
+
+func (self *MscfbFileInfo) IsDir() bool {
+	return self.entry.IsDir
+}
+
+func (self *MscfbFileInfo) Data() *ordereddict.Dict {
+	return ordereddict.NewDict()
+}
+
+func (self *MscfbFileInfo) FullPath() string {
+	return self._full_path.String()
+}
+
+func (self *MscfbFileInfo) OSPath() *accessors.OSPath {
+	return self._full_path
+}
+
+// Not supported
+func (self *MscfbFileInfo) IsLink() bool {
+	return false
+}
+
+func (self *MscfbFileInfo) GetLink() (*accessors.OSPath, error) {
+	return nil, errors.New("Not implemented")
+}
+
+func (self *MscfbFileInfo) Mtime() time.Time {
+	return self.entry.Mtime
+}
+
+func (self *MscfbFileInfo) ModTime() time.Time {
+	return self.entry.Mtime
+}
+
+func (self *MscfbFileInfo) Atime() time.Time {
+	return time.Time{}
+}
+
+func (self *MscfbFileInfo) Ctime() time.Time {
+	return self.entry.Ctime
+}
+
+func (self *MscfbFileInfo) Btime() time.Time {
+	return self.entry.Ctime
+}
+
+func (self *MscfbFileInfo) Size() int64 {
+	return int64(self.entry.Size)
+}
+
+func (self *MscfbFileInfo) Mode() os.FileMode {
+	var result os.FileMode = 0755
+	if self.IsDir() {
+		result |= os.ModeDir
+	}
+	return result
+}
+
+type MscfbFileSystemAccessor struct {
+	scope vfilter.Scope
+
+	// The delegate accessor we use to open the underlying volume.
+	accessor string
+	device   *accessors.OSPath
+
+	root *accessors.OSPath
+}
+
+func NewMscfbFileSystemAccessor(
+	scope vfilter.Scope,
+	root_path *accessors.OSPath,
+	device *accessors.OSPath, accessor string) *MscfbFileSystemAccessor {
+	return &MscfbFileSystemAccessor{
+		scope:    scope,
+		accessor: accessor,
+		device:   device,
+		root:     root_path,
+	}
+}
+
+func (self MscfbFileSystemAccessor) New(scope vfilter.Scope) (
+	accessors.FileSystemAccessor, error) {
+	// Create a new cache in the scope.
+	return &MscfbFileSystemAccessor{
+		scope:    scope,
+		device:   self.device,
+		accessor: self.accessor,
+		root:     self.root,
+	}, nil
+}
+
+func (self MscfbFileSystemAccessor) ParsePath(path string) (
+	*accessors.OSPath, error) {
+	return accessors.NewLinuxOSPath(path)
+}
+
+func (self *MscfbFileSystemAccessor) ReadDir(path string) (
+	res []accessors.FileInfo, err error) {
+	// Normalize the path
+	fullpath, err := self.ParsePath(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return self.ReadDirWithOSPath(fullpath)
+}
+
+func (self *MscfbFileSystemAccessor) ReadDirWithOSPath(
+	fullpath *accessors.OSPath) (res []accessors.FileInfo, err error) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			fmt.Printf("PANIC %v\n", r)
+			debug.PrintStack()
+			err, _ = r.(error)
+		}
+	}()
+
+	result := []accessors.FileInfo{}
+	if len(fullpath.Components) > 0 {
+		return nil, errors.New("Not found error")
+	}
+
+	ole_ctx, err := GetMscfbContext(
+		self.scope, self.device, fullpath, self.accessor)
+	if err != nil {
+		return nil, err
+	}
+
+	// List the directory.
+	for _, info := range ole_ctx.Directories {
+		result = append(result, &MscfbFileInfo{
+			entry:      &info,
+			_full_path: fullpath.Append(info.Name),
+		})
+	}
+	return result, nil
+}
+
+func (self *MscfbFileSystemAccessor) Open(
+	path string) (res accessors.ReadSeekCloser, err error) {
+
+	full_path, err := self.ParsePath(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return self.OpenWithOSPath(full_path)
+}
+
+func (self *MscfbFileSystemAccessor) OpenWithOSPath(
+	fullpath *accessors.OSPath) (res accessors.ReadSeekCloser, err error) {
+
+	defer func() {
+		r := recover()
+		if r != nil {
+			fmt.Printf("PANIC %v\n", r)
+			debug.PrintStack()
+			err, _ = r.(error)
+		}
+	}()
+
+	if len(fullpath.Components) != 1 {
+		return nil, errors.New("Not found error")
+	}
+
+	ole_ctx, err := GetMscfbContext(
+		self.scope, self.device, fullpath, self.accessor)
+	if err != nil {
+		return nil, err
+	}
+
+	// Open the device path from the root.
+	stream, dir, err := ole_ctx.Open(fullpath.Components[0])
+	if err != nil {
+		return nil, err
+	}
+
+	return &readAdapter{
+		info: &MscfbFileInfo{
+			entry:      dir,
+			_full_path: fullpath,
+		},
+		reader: stream,
+	}, nil
+}
+
+func (self *MscfbFileSystemAccessor) Lstat(
+	path string) (res accessors.FileInfo, err error) {
+
+	fullpath, err := self.ParsePath(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return self.LstatWithOSPath(fullpath)
+}
+
+func (self *MscfbFileSystemAccessor) LstatWithOSPath(
+	fullpath *accessors.OSPath) (res accessors.FileInfo, err error) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			fmt.Printf("PANIC %v\n", r)
+			debug.PrintStack()
+			err, _ = r.(error)
+		}
+	}()
+
+	ole_ctx, err := GetMscfbContext(
+		self.scope, self.device, fullpath, self.accessor)
+	if err != nil {
+		return nil, err
+	}
+
+	var dir *parser.DirectoryEntry
+	if len(fullpath.Components) > 1 {
+		return nil, errors.New("Not found error")
+	}
+
+	if len(fullpath.Components) == 0 {
+		// Root directory
+		dir, err = ole_ctx.GetDirentry(0)
+	} else {
+
+		dir, err = ole_ctx.Stat(fullpath.Components[0])
+	}
+
+	return &MscfbFileInfo{
+		entry:      dir,
+		_full_path: fullpath,
+	}, err
+}
+
+func init() {
+	accessors.Register("mscfb", &MscfbFileSystemAccessor{},
+		`Parse a MSCFB file as an archive.
+`)
+
+	json.RegisterCustomEncoder(&MscfbFileInfo{}, accessors.MarshalGlobFileInfo)
+}
---
+++ accessors/mscfb/utils.go
@@ -0,0 +1,56 @@
+package mscfb
+
+import (
+	"github.com/Velocidex/go-mscfb/parser"
+	"www.velocidex.com/golang/velociraptor/accessors"
+	"www.velocidex.com/golang/velociraptor/constants"
+	vql_subsystem "www.velocidex.com/golang/velociraptor/vql"
+	"www.velocidex.com/golang/velociraptor/vql/readers"
+	"www.velocidex.com/golang/vfilter"
+)
+
+func GetMscfbContext(scope vfilter.Scope,
+	device, fullpath *accessors.OSPath, accessor string) (
+	result *parser.OLEContext, err error) {
+
+	if device == nil {
+		device, err = fullpath.Delegate(scope)
+		if err != nil {
+			return nil, err
+		}
+		accessor = fullpath.DelegateAccessor()
+	}
+
+	return GetMscfbCache(scope, device, accessor)
+}
+
+func GetMscfbCache(scope vfilter.Scope,
+	device *accessors.OSPath, accessor string) (*parser.OLEContext, error) {
+	key := "mscfb_cache" + device.String() + accessor
+
+	// Get the cache context from the root scope's cache
+	cache_ctx, ok := vql_subsystem.CacheGet(scope, key).(*parser.OLEContext)
+	if !ok {
+		lru_size := vql_subsystem.GetIntFromRow(
+			scope, scope, constants.NTFS_CACHE_SIZE)
+
+		paged_reader, err := readers.NewAccessorReader(
+			scope, accessor, device, int(lru_size))
+
+		cache_ctx, err = parser.GetOLEContext(paged_reader)
+		if err != nil {
+			return nil, err
+		}
+		vql_subsystem.CacheSet(scope, key, cache_ctx)
+
+		// Close the device when we are done with this query.
+		err = vql_subsystem.GetRootScope(scope).AddDestructor(func() {
+			paged_reader.Close()
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return cache_ctx, nil
+}
