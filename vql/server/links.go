4d0135b5da2537db43a78f2af334392214dbaaba
Mike Cohen <scudette@users.noreply.github.com>
2024-10-12 00:20:04 +1000

    Bugfix: Fixed broken links in dashboard (#3817)
    Extended the link_to() VQL function to support more objects, such as
    uploads.
    
    Also fixed timezone issues in graphs.

--- vql/server/links.go
+++ vql/server/links.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/Velocidex/ordereddict"
+	"www.velocidex.com/golang/velociraptor/services"
 	"www.velocidex.com/golang/velociraptor/vql"
 	vql_subsystem "www.velocidex.com/golang/velociraptor/vql"
 	"www.velocidex.com/golang/vfilter"
@@ -14,15 +15,18 @@ import (
 )
 
 type LinkToFunctionArgs struct {
-	Type     string `vfilter:"optional,field=type,doc=The type of link. Currently one of collection, hunt, artifact, event"`
-	ClientId string `vfilter:"optional,field=client_id"`
-	FlowId   string `vfilter:"optional,field=flow_id"`
-	Tab      string `vfilter:"optional,field=tab,doc=The tab to focus - can be overview, request, results, logs, notebook"`
-	Text     string `vfilter:"optional,field=text,doc=If specified we emit a markdown style URL with a text"`
+	Type     string            `vfilter:"optional,field=type,doc=The type of link. Currently one of collection, hunt, artifact, event"`
+	ClientId string            `vfilter:"optional,field=client_id"`
+	FlowId   string            `vfilter:"optional,field=flow_id"`
+	Upload   *ordereddict.Dict `vfilter:"optional,field=upload,doc=Upload object for the file to upload (upload object is returned by the upload() function)"`
+	Tab      string            `vfilter:"optional,field=tab,doc=The tab to focus - can be overview, request, results, logs, notebook"`
+	Text     string            `vfilter:"optional,field=text,doc=If specified we emit a markdown style URL with a text"`
 
 	HuntId   string `vfilter:"optional,field=hunt_id,doc=The hunt id to read."`
 	Artifact string `vfilter:"optional,field=artifact,doc=The artifact to retrieve"`
-	OrgId    string `vfilter:"optional,field=org,doc=If set the link accesses a different org. Otherwise we accesses the current org."`
+
+	RawLink bool   `vfilter:"optional,field=raw,doc=When specified we emit a raw URL (without autodetected text)"`
+	OrgId   string `vfilter:"optional,field=org,doc=If set the link accesses a different org. Otherwise we accesses the current org."`
 }
 
 type LinkToFunction struct{}
@@ -44,15 +48,13 @@ func (self *LinkToFunction) Call(ctx context.Context,
 		return vfilter.Null{}
 	}
 
-	if config_obj.GUI.PublicUrl == "" {
-		scope.Log("link_to: Invalid configuration! GUI.public_url must be the public URL over which the GUI is served!")
-		return vfilter.Null{}
-	}
-
-	url, err := url.Parse(config_obj.GUI.PublicUrl)
-	if err != nil {
-		scope.Log("link_to: Invalid configuration! GUI.public_url must be the public URL over which the GUI is served!: %v", err)
-		return vfilter.Null{}
+	url := &url.URL{Path: "/"}
+	if config_obj.GUI.PublicUrl != "" {
+		url, err = url.Parse(config_obj.GUI.PublicUrl)
+		if err != nil {
+			scope.Log("link_to: Invalid configuration! GUI.public_url must be the public URL over which the GUI is served!: %v", err)
+			return vfilter.Null{}
+		}
 	}
 
 	org := arg.OrgId
@@ -61,13 +63,64 @@ func (self *LinkToFunction) Call(ctx context.Context,
 	}
 
 	query := url.Query()
-	if !query.Has("org_id") {
-		query.Set("org_id", org)
-		url.RawQuery = query.Encode()
+
+	// Autodetect the type based on the args
+	if arg.Type == "" {
+		if arg.ClientId != "" && arg.FlowId != "" {
+			arg.Type = "collection"
+		} else if arg.ClientId != "" {
+			arg.Type = "client"
+		} else if arg.HuntId != "" {
+			arg.Type = "hunt"
+		} else if arg.Artifact != "" && arg.ClientId != "" {
+			arg.Type = "event"
+		} else if arg.Artifact != "" {
+			arg.Type = "artifact"
+		} else if arg.Upload != nil {
+			arg.Type = "upload"
+		} else {
+			scope.Log("link_to: Supported link types must be one of client, collection, hunt, artifact, event")
+			return vfilter.Null{}
+		}
 	}
 
 	switch strings.ToLower(arg.Type) {
-	case "", "collection":
+	case "upload":
+		components, pres := arg.Upload.GetStrings("Components")
+		if !pres {
+			scope.Log("link_to: upload link does not have a components field")
+			return vfilter.Null{}
+		}
+
+		vfs_name, pres := arg.Upload.GetString("StoredName")
+		if !arg.RawLink && pres && arg.Text == "" {
+			arg.Text = vfs_name
+		}
+
+		// The link is an API call to VFSDownloadInfo
+		url.Path = "/api/v1/DownloadVFSFile"
+		query.Add("vfs_path", vfs_name)
+		for _, c := range components {
+			query.Add("fs_components", c)
+		}
+		return formatURL(arg.Text, url, query, org)
+
+	case "client":
+		if arg.ClientId == "" {
+			scope.Log("link_to: For client link client_id must be set")
+			return vfilter.Null{}
+		}
+
+		// Default label includes the hostname
+		if !arg.RawLink && arg.Text == "" {
+			arg.Text = fmt.Sprintf("%v (%v)", arg.ClientId, services.GetHostname(
+				ctx, config_obj, arg.ClientId))
+		}
+
+		url.Fragment = fmt.Sprintf("/host/%v", arg.ClientId)
+		return formatURL(arg.Text, url, query, org)
+
+	case "collection":
 		if arg.ClientId == "" || arg.FlowId == "" {
 			scope.Log("link_to: For collection link both client_id and flow_id must be set")
 			return vfilter.Null{}
@@ -77,8 +130,14 @@ func (self *LinkToFunction) Call(ctx context.Context,
 		if tab == "" {
 			tab = "overview"
 		}
-		url.Fragment = fmt.Sprintf("/collected/%v/%v/%v", arg.ClientId, arg.FlowId, tab)
-		return formatURL(arg.Text, url)
+
+		if !arg.RawLink && arg.Text == "" {
+			arg.Text = arg.FlowId
+		}
+
+		url.Fragment = fmt.Sprintf("/collected/%v/%v/%v",
+			arg.ClientId, arg.FlowId, tab)
+		return formatURL(arg.Text, url, query, org)
 
 	case "hunt":
 		if arg.HuntId == "" {
@@ -90,8 +149,13 @@ func (self *LinkToFunction) Call(ctx context.Context,
 		if tab == "" {
 			tab = "overview"
 		}
+
+		if !arg.RawLink && arg.Text == "" {
+			arg.Text = arg.HuntId
+		}
+
 		url.Fragment = fmt.Sprintf("/hunts/%v/%v", arg.HuntId, tab)
-		return formatURL(arg.Text, url)
+		return formatURL(arg.Text, url, query, org)
 
 	case "artifact":
 		if arg.Artifact == "" {
@@ -99,8 +163,12 @@ func (self *LinkToFunction) Call(ctx context.Context,
 			return vfilter.Null{}
 		}
 
+		if !arg.RawLink && arg.Text == "" {
+			arg.Text = arg.Artifact
+		}
+
 		url.Fragment = fmt.Sprintf("/artifacts/%v", arg.Artifact)
-		return formatURL(arg.Text, url)
+		return formatURL(arg.Text, url, query, org)
 
 	case "event":
 		if arg.Artifact == "" || arg.ClientId == "" {
@@ -108,16 +176,30 @@ func (self *LinkToFunction) Call(ctx context.Context,
 			return vfilter.Null{}
 		}
 
+		if !arg.RawLink && arg.Text == "" {
+			arg.Text = arg.Artifact
+		}
+
 		url.Fragment = fmt.Sprintf("/events/%v/%v", arg.ClientId, arg.Artifact)
-		return formatURL(arg.Text, url)
+		return formatURL(arg.Text, url, query, org)
 
 	default:
-		scope.Log("link_to: Supported link types must be one of collection, hunt, artifact, event")
+		scope.Log("link_to: Supported link types must be one of client, collection, hunt, artifact, event")
 		return vfilter.Null{}
 	}
 }
 
-func formatURL(text string, url *url.URL) string {
+func formatURL(text string, url *url.URL, query url.Values, org string) string {
+	// By convention we need to add the org_id at the end.
+	if org == "" {
+		org = "root"
+	}
+
+	if !query.Has("org_id") {
+		query.Set("org_id", org)
+	}
+
+	url.RawQuery = query.Encode()
 	if text == "" {
 		return url.String()
 	}
