3a4f6a91bf1d233880083dfe64def87032c8bfd4
Mike Cohen <scudette@users.noreply.github.com>
2025-01-01 22:41:53 +1000

    Bugfix: yara_lint() should remove invalid imports from yara rules. (#3987)

--- api/authenticators/oidc.go
+++ api/authenticators/oidc.go
@@ -11,7 +11,6 @@ import (
 	api_utils "www.velocidex.com/golang/velociraptor/api/utils"
 	config_proto "www.velocidex.com/golang/velociraptor/config/proto"
 	"www.velocidex.com/golang/velociraptor/logging"
-	"www.velocidex.com/golang/velociraptor/utils"
 )
 
 type OIDCConnector interface {
@@ -139,8 +138,6 @@ func (self *OidcAuthenticator) oauthOidcLogin(
 				http.Error(w, "rejected", http.StatusUnauthorized)
 			}
 			oidcOauthConfig.Endpoint = provider.Endpoint()
-
-			utils.Debug(oidcOauthConfig)
 
 			// Create oauthState cookie
 			oauthState, err := r.Cookie("oauthstate")
--- gui/velociraptor/src/components/core/paged-table.jsx
+++ gui/velociraptor/src/components/core/paged-table.jsx
@@ -667,7 +667,10 @@ class VeloPagedTable extends Component {
         if(_.isEmpty(transform) && !_.isEmpty(this.props.transform)) {
             Object.assign(transform, this.props.transform);
         }
+        return transform;
+    }
 
+    getTransformedRenderer = (transform)=>{
         if(_.isEmpty(transform)) {
             return <></>;
         }
@@ -851,6 +854,16 @@ class VeloPagedTable extends Component {
             downloads.columns = active_columns;
         }
 
+        if(transformed.filter_column) {
+            downloads.filter_column = transformed.filter_column;
+            downloads.filter_regex = transformed.filter_regex;
+        }
+
+        if (transformed.sort_column) {
+            downloads.sort_column = transformed.sort_column;
+            downloads.sort_direction = transformed.sort_direction;
+        }
+
         let all_compacted = true;
         let none_compacted = true;
 
@@ -924,7 +937,7 @@ class VeloPagedTable extends Component {
 
               </ButtonGroup>
                 <ButtonGroup className="float-right">
-                  { transformed }
+                  { this.getTransformedRenderer(transformed) }
                 </ButtonGroup>
               { this.props.toolbar || <></> }
             </Navbar>
