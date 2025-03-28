bd1f9fa346eff24c5834333c769589fdc556ce59
Mike Cohen <mike@velocidex.com>
2025-03-28 12:03:58 +1000

    Bugfix: Fix deadlock in profile artifact.
    This would deadlock a client's profile flow.

--- constants/constants.go
+++ constants/constants.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	VERSION = "0.74.0-rc1"
+	VERSION = "0.74.1"
 
 	// This is the version of dependent client binaries that will be
 	// included in the offline collector or MSI. Usually this will be
