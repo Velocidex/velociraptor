274e1bf51d9bdc7909c2d535147668d807534321
Mike Cohen <scudette@users.noreply.github.com>
2025-04-01 13:14:13 +1000

    Bugfix: UpdateNotebookCell raced periodic update (#4151)
    In rare situations the periodic update mechanism. This resulted in
    notebook cell state going backwards preventing proper updates.

--- constants/constants.go
+++ constants/constants.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	VERSION = "0.74.1"
+	VERSION = "0.74.2"
 
 	// This is the version of dependent client binaries that will be
 	// included in the offline collector or MSI. Usually this will be
