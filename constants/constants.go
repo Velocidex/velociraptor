724075200caef841af513809080ab5153b7d7ee6
Mike Cohen <scudette@users.noreply.github.com>
2025-06-26 10:24:54 +1000

    Improve Client Monitoring tracker to include finish time. (#4316)

--- constants/constants.go
+++ constants/constants.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	VERSION = "0.74.3"
+	VERSION = "0.74.4"
 
 	// This is the version of dependent client binaries that will be
 	// included in the offline collector or MSI. Usually this will be
