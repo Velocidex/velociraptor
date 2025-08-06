b6a9b86f06c77b323eb5f9b7e1560c290fd39217
Tim Dithmer <dith.tim@gmail.com>
2025-08-06 11:56:15 +0200

    Remove unnecessary 'not /' characters from basePathRegEx

--- services/sanity/frontend.go
+++ services/sanity/frontend.go
@@ -11,7 +11,7 @@ import (
 )
 
 var (
-	basePathRegEx = regexp.MustCompile("^/[^/][a-zA-Z0-9_-]+[^/]$")
+	basePathRegEx = regexp.MustCompile("^/[a-zA-Z0-9_-]$")
 )
 
 func (self *SanityChecks) CheckDatastoreSettings(
