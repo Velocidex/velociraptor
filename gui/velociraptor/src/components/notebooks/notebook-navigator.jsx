2a7383b87f004608ffe1f3f9372beb715f7e3997
Mike Cohen <scudette@users.noreply.github.com>
2025-12-04 16:49:52 +1000

    Added a notebook navigator (#4574)
    This makes it easier to got to different cells in long notebooks with
    many cells.
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ gui/velociraptor/src/components/notebooks/notebook-navigator.jsx
@@ -0,0 +1,113 @@
+import _ from 'lodash';
+
+import "./notebook-navigator.css";
+import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import T from '../i8n/i8n.jsx';
+import VeloTable, { getFormatter } from '../core/table.jsx';
+import VeloTimestamp from "../utils/time.jsx";
+import classNames from "classnames";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { withRouter }  from "react-router-dom";
+
+class NotebookNavigator extends Component {
+    static propTypes = {
+        notebook: PropTypes.object,
+        scrollToCell: PropTypes.func,
+        match: PropTypes.object,
+        history: PropTypes.object,
+    };
+
+    state = {
+        collapsed: true,
+    }
+
+    linkToCell = (cell, row)=>{
+        return <Button
+                 onClick={x=>{
+                     this.props.scrollToCell(cell);
+                     this.setState({collapsed: true});
+                 }}
+               >
+                 {cell}
+               </Button>;
+    }
+
+    setFullScreen = () => {
+        if (this.props.notebook &&
+            this.props.notebook.notebook_id) {
+
+            console.log(this.props.match);
+            if (this.props.match.path.startsWith("/fullscreen")) {
+                this.props.history.push(
+                    "/notebooks/" +
+                        this.props.notebook.notebook_id);
+            } else {
+                this.props.history.push(
+                    "/fullscreen/notebooks/" +
+                        this.props.notebook.notebook_id);
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className="float-left navigator">
+              <div
+                className={classNames({
+                    collapsed: this.state.collapsed,
+                    uncollapsed: !this.state.collapsed,
+                    notebook_nav: true,
+                })}
+                onClick={this.collapse}
+              >
+                <div>
+                <nav className="navigator" aria-labelledby="mainmenu">
+                  <ul className="nav nav-pills navigator">
+                    <li className="nav-link">
+                      <Button
+                        onClick={this.setFullScreen}
+                        variant='outline-default'>
+                        <i className="navicon">
+                          <FontAwesomeIcon icon="expand" />
+                        </i>
+                      </Button>
+                    </li>
+                    <li className="nav-link">
+                      <Button
+                        onClick={x=>this.setState({
+                            collapsed: !this.state.collapsed})}
+                        variant='outline-default'>
+                          <i className="navicon">
+                            <FontAwesomeIcon icon="list" />
+                          </i>
+                      </Button>
+                      <div className="link-description">
+                        {T("Overview")}
+                      </div>
+                    </li>
+                    <span className="notebook-outline">
+                      <VeloTable
+                        rows={this.props.notebook.cell_metadata}
+                        columns={["cell_id", "timestamp", "type", "summary"]}
+                        no_toolbar={true}
+                        header_renderers={{"cell_id": T("Cell"),
+                                           "timestamp": T("Timestamp"),
+                                           "type": T("Type")}}
+                        column_renderers={{
+                            "cell_id": this.linkToCell,
+                            "timestamp": getFormatter("timestamp")}}
+                      />
+                    </span>
+                  </ul>
+                </nav>
+            </div>
+            </div>
+            </div>
+        );
+    }
+}
+
+
+export default withRouter(NotebookNavigator);
--- services/notebook/utils.go
+++ services/notebook/utils.go
@@ -1,6 +1,8 @@
 package notebook
 
 import (
+	"regexp"
+
 	api_proto "www.velocidex.com/golang/velociraptor/api/proto"
 	"www.velocidex.com/golang/velociraptor/utils"
 )
@@ -21,10 +23,29 @@ func GetNextVersion(version string) string {
 	return utils.NextId()
 }
 
+var (
+	summary_regex = []*regexp.Regexp{
+		// The first heading is a good summary.
+		regexp.MustCompile("<h[1-4]>(.+?)</h"),
+	}
+)
+
 // Not all values from the real cell are stored in cell summaries.
 func SummarizeCell(cell_md *api_proto.NotebookCell) *api_proto.NotebookCell {
+	var summary string
+
+	// Derive the summary of the cell by its output.
+	for _, re := range summary_regex {
+		matches := re.FindStringSubmatch(cell_md.Output)
+		if len(matches) > 1 {
+			summary = matches[1]
+			break
+		}
+	}
+
 	return &api_proto.NotebookCell{
 		CellId:            cell_md.CellId,
+		Summary:           summary,
 		Timestamp:         cell_md.Timestamp,
 		Type:              cell_md.Type,
 		CurrentVersion:    cell_md.CurrentVersion,
