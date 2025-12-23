5a2aa43e76ee88ca8b9f3a2a7bcb3f0d8575cf34
Mike Cohen <mike@velocidex.com>
2025-12-23 10:41:13 +1000

    Prepare for 0.75.6 release

--- constants/constants.go
+++ constants/constants.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	VERSION = "0.75.5"
+	VERSION = "0.75.6"
 
 	// This is the version of dependent client binaries that will be
 	// included in the offline collector or MSI. Usually this will be
