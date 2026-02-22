d71a51060bfed29bdee69e207268a1181ec274c0
nullifysecurity <nullify@nullify.dev>
2026-02-22 22:37:02 +1100

    refactor(cli): modify reformat command to wrap vql

--- bin/reformat.go
+++ bin/reformat.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 
+	"github.com/Velocidex/ordereddict"
 	"www.velocidex.com/golang/velociraptor/config"
-	"www.velocidex.com/golang/velociraptor/constants"
 	logging "www.velocidex.com/golang/velociraptor/logging"
 	"www.velocidex.com/golang/velociraptor/services"
 	"www.velocidex.com/golang/velociraptor/startup"
-	"www.velocidex.com/golang/velociraptor/utils"
+	"www.velocidex.com/golang/velociraptor/vql/acl_managers"
+	"www.velocidex.com/golang/velociraptor/vql/functions"
+	"www.velocidex.com/golang/vfilter"
 )
 
 var (
@@ -47,38 +51,81 @@ func doReformat() error {
 
 	// Report all errors and keep going as much as possible.
 	returned_errs := make(map[string]error)
+	var artifact_paths []string
 
 	for _, artifact_path := range *reformat_args {
-		returned_errs[artifact_path] = nil
-
-		fd, err := os.Open(artifact_path)
+		abs, err := filepath.Abs(artifact_path)
 		if err != nil {
-			returned_errs[artifact_path] = err
+			logger.Error("reformat: could not get absolute path for %v", artifact_path)
 			continue
 		}
 
-		data, err := utils.ReadAllWithLimit(fd, constants.MAX_MEMORY)
-		if err != nil {
-			returned_errs[artifact_path] = err
-			fd.Close()
-			continue
-		}
-		fd.Close()
+		artifact_paths = append(artifact_paths, abs)
+	}
 
-		reformatted, err := manager.ReformatVQL(ctx, string(data))
-		if err != nil {
-			returned_errs[artifact_path] = err
-			continue
-		}
+	artifact_logger := &LogWriter{config_obj: sm.Config}
+	builder := services.ScopeBuilder{
+		Config:     sm.Config,
+		ACLManager: acl_managers.NewRoleACLManager(sm.Config, "administrator"),
+		Logger:     log.New(artifact_logger, "", 0),
+		Env: ordereddict.NewDict().
+			Set("Artifacts", artifact_paths),
+	}
 
-		out_fd, err := os.OpenFile(artifact_path,
-			os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0600)
-		if err != nil {
-			returned_errs[artifact_path] = err
-			continue
+	query := `
+		SELECT FilePath,
+			   Result
+		FROM foreach(row=Artifacts,
+					 query={
+			SELECT _value AS FilePath,
+				   reformat(artifact=read_file(filename=_value)) AS Result
+			FROM scope()
+		})
+	`
+
+	scope := manager.BuildScope(builder)
+	defer scope.Close()
+
+	statements, err := vfilter.MultiParse(query)
+	if err != nil {
+		logger.Error("reformat: error parsing query: %v", query)
+		return err
+	}
+
+	for _, vql := range statements {
+		for row := range vql.Eval(sm.Ctx, scope) {
+			dict := vfilter.RowToDict(ctx, scope, row)
+
+			artifact_path, pres := dict.GetString("FilePath")
+			if !pres {
+				continue
+			}
+
+			result_val, pres := dict.Get("Result")
+			if !pres {
+				continue
+			}
+
+			result, ok := result_val.(*functions.ReformatFunctionResult)
+			if !ok {
+				continue
+			}
+
+			if result.Error != nil {
+				returned_errs[artifact_path] = result.Error
+				continue
+			}
+
+			out_fd, err := os.OpenFile(artifact_path, os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0600)
+			if err != nil {
+				returned_errs[artifact_path] = err
+				continue
+			}
+			_, _ = out_fd.Write([]byte(result.Artifact))
+			out_fd.Close()
+
+			returned_errs[artifact_path] = nil
 		}
-		_, _ = out_fd.Write([]byte(reformatted))
-		out_fd.Close()
 	}
 
 	var ret error
