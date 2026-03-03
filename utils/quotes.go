5c465a3e9b4ba09837bf0153984f4c12ec77c23d
araesa <a.raesa@student.uq.edu.au>
2026-03-03 15:11:08 +1000

    Tighten quote stripping to absolute paths only

--- utils/quotes.go
+++ utils/quotes.go
@@ -14,8 +14,13 @@ func UnQuote(in string) string {
 	return res
 }
 
+// MaybeStripWrappingQuotes removes wrapping single quotes from absolute
+// paths produced by macOS Finder's "Copy as Pathname" (Sequoia+).
+// Finder only wraps in quotes when the path has spaces and no single
+// quotes; paths containing literal single quotes use backslash escaping
+// instead, so this is unambiguous.
 func MaybeStripWrappingQuotes(path string) string {
-	if len(path) >= 2 && path[0] == '\'' && path[len(path)-1] == '\'' {
+	if len(path) >= 3 && path[0] == '\'' && path[len(path)-1] == '\'' && path[1] == '/' {
 		return path[1 : len(path)-1]
 	}
 	return path
