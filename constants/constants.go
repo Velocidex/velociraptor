d801ec61c411c02966157150bade892cc91f6762
Mike Cohen <scudette@users.noreply.github.com>
2026-03-21 18:26:03 +1000

    Rework command line parser for run mode. (#4749)
    Also incorporate fix for ESE parser

--- constants/constants.go
+++ constants/constants.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	VERSION = "0.76.1-rc1"
+	VERSION = "0.76.1"
 
 	// This is the version of dependent client binaries that will be
 	// included in the offline collector or MSI. Usually this will be
