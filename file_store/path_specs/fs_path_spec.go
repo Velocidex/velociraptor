2f11aadf3e6f6eb0493eada748b587d586fe6fa9
Mike Cohen <scudette@users.noreply.github.com>
2026-06-17 17:02:02 +1000

    Serialize writes on filestore files. (#4887)
    This protects filestore files from corruption when multiple goroutines
    are writing to the same file at the same time.
    
    Although this event is rare it is worth protecting against it.
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- file_store/path_specs/fs_path_spec.go
+++ file_store/path_specs/fs_path_spec.go
@@ -2,82 +2,95 @@ package path_specs
 
 import (
 	"strconv"
+	"sync"
 
 	"www.velocidex.com/golang/velociraptor/file_store/api"
 	"www.velocidex.com/golang/velociraptor/utils"
 )
 
 type FSPathSpec struct {
-	DSPathSpec
+	*DSPathSpec
 
 	tag string
+
+	mu                 sync.Mutex
+	cached_client_path *string
 }
 
-func (self FSPathSpec) Tag() string {
+func (self *FSPathSpec) Tag() string {
 	return self.tag
 }
 
-func (self FSPathSpec) SetTag(tag string) api.FSPathSpec {
+func (self *FSPathSpec) SetTag(tag string) api.FSPathSpec {
 	self.tag = tag
 	return self
 }
 
-func (self FSPathSpec) String() string {
+func (self *FSPathSpec) String() string {
 	return "fs:" + self.AsClientPath()
 }
 
-func (self FSPathSpec) Dir() api.FSPathSpec {
+func (self *FSPathSpec) Dir() api.FSPathSpec {
 	new_components := utils.CopySlice(self.components)
 	if len(new_components) > 0 {
 		new_components = new_components[:len(new_components)-1]
 	}
-	return FSPathSpec{DSPathSpec: DSPathSpec{
+	return &FSPathSpec{DSPathSpec: &DSPathSpec{
 		components: new_components,
 		path_type:  self.path_type,
 	}}
 }
 
-func (self FSPathSpec) MarshalJSON() ([]byte, error) {
+func (self *FSPathSpec) MarshalJSON() ([]byte, error) {
 	return []byte(strconv.Quote("fs:" + self.AsClientPath())), nil
 }
 
-func (self FSPathSpec) AsDatastorePath() api.DSPathSpec {
+func (self *FSPathSpec) AsDatastorePath() api.DSPathSpec {
 	return self.DSPathSpec.
 		SetType(api.PATH_TYPE_DATASTORE_JSON)
 }
 
 // Adds an unsafe component to this path.
-func (self FSPathSpec) AddChild(child ...string) api.FSPathSpec {
-	return FSPathSpec{DSPathSpec: DSPathSpec{
+func (self *FSPathSpec) AddChild(child ...string) api.FSPathSpec {
+	return &FSPathSpec{DSPathSpec: &DSPathSpec{
 		components: append(utils.CopySlice(self.components), child...),
 		path_type:  self.path_type,
 		is_safe:    self.is_safe,
 	}}
 }
 
-func (self FSPathSpec) AddUnsafeChild(child ...string) api.FSPathSpec {
-	return FSPathSpec{DSPathSpec: DSPathSpec{
+func (self *FSPathSpec) AddUnsafeChild(child ...string) api.FSPathSpec {
+	return &FSPathSpec{DSPathSpec: &DSPathSpec{
 		components: append(utils.CopySlice(self.components), child...),
 		path_type:  self.path_type,
 		is_safe:    false,
 	}}
 }
 
-func (self FSPathSpec) SetType(ext api.PathType) api.FSPathSpec {
-	return FSPathSpec{DSPathSpec: DSPathSpec{
+func (self *FSPathSpec) SetType(ext api.PathType) api.FSPathSpec {
+	return &FSPathSpec{DSPathSpec: &DSPathSpec{
 		components: self.components,
 		path_type:  ext,
 		is_safe:    self.is_safe,
 	}}
 }
 
-func (self FSPathSpec) AsClientPath() string {
-	return utils.JoinComponents(self.components, "/") +
+func (self *FSPathSpec) AsClientPath() string {
+	self.mu.Lock()
+	defer self.mu.Unlock()
+
+	if self.cached_client_path != nil {
+		return *self.cached_client_path
+	}
+
+	res := utils.JoinComponents(self.components, "/") +
 		api.GetExtensionForFilestore(self)
+	self.cached_client_path = &res
+	return res
 }
 
 func NewUnsafeFilestorePath(path_components ...string) api.FSPathSpec {
-	result := FSPathSpec{DSPathSpec: DSPathSpec{
+	result := &FSPathSpec{DSPathSpec: &DSPathSpec{
 		components: path_components,
 		// By default write JSON files.
 		path_type: api.PATH_TYPE_FILESTORE_JSON,
@@ -88,7 +101,7 @@ func NewUnsafeFilestorePath(path_components ...string) api.FSPathSpec {
 }
 
 func NewSafeFilestorePath(path_components ...string) api.FSPathSpec {
-	result := FSPathSpec{DSPathSpec: DSPathSpec{
+	result := &FSPathSpec{DSPathSpec: &DSPathSpec{
 		components: path_components,
 		path_type:  api.PATH_TYPE_FILESTORE_JSON,
 		is_safe:    true,
