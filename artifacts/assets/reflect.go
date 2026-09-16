36127922de905f1fb4b4279e2af443ffacba6ef3
Mike Cohen <mike@velocidex.com>
2026-09-16 12:42:20 +1000

    Fix build

---
+++ artifacts/assets/reflect.go
@@ -0,0 +1,18 @@
+package assets
+
+import (
+	"github.com/Velocidex/yaml/v2"
+	api_proto "www.velocidex.com/golang/velociraptor/api/proto"
+)
+
+// Loads the api description from the embedded asset
+func LoadApiDescription() ([]*api_proto.Completion, error) {
+	data, err := ReadFile("/docs/references/vql.yaml")
+	if err != nil {
+		return nil, err
+	}
+
+	result := []*api_proto.Completion{}
+	err = yaml.Unmarshal(data, &result)
+	return result, err
+}
