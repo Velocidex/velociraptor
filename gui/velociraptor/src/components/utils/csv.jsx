423c3c36472990e87c11f072eecbf8e7ae92a4c5
Mike Cohen <mike@velocidex.com>
2026-09-17 23:45:45 +1000

    Fixed spelling

--- gui/velociraptor/src/components/utils/csv.jsx
+++ gui/velociraptor/src/components/utils/csv.jsx
@@ -1,5 +1,5 @@
 // Pin csv-parse to v4 - later versions import node built in which are
-// not polifilled on client code.
+// not polyfill on client code.
 import parse from 'csv-parse/lib/browser/sync.js';
 import stringify from 'csv-stringify/lib/browser/sync.js';
 import api from '../core/api-service.jsx';
